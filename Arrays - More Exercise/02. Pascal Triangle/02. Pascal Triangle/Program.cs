85dd86b2cb67f55be79ba80198a2ea1cba29bcef
Georgi Dimitrov <43094653+Wafrey@users.noreply.github.com>
2019-02-10 20:49:12 +0200

    Add files via upload

---
+++ Arrays - More Exercise/02. Pascal Triangle/02. Pascal Triangle/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace _02._Pascal_Triangle
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int row = int.Parse(Console.ReadLine());
+            int[] array = new int[row];
+            int[] currentArray = new int[row];
+            array[0] = 1;
+
+            for (int i = 0; i < row; i++)
+            {
+
+                for (int s = 0; s < array.Length; s++)
+                {
+                    currentArray[s] = array[s];
+                }
+                for (int k = 1; k <= i; k++)
+                {
+                    array[0] = 1;
+                    array[k] = currentArray[k - 1] + currentArray[k];
+                }
+                for (int l = 0; l <= i; l++)
+                {
+                    Console.Write(array[l] + " ");
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
