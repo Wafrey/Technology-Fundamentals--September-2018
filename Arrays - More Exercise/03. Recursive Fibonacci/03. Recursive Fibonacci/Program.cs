958554f59eaaad5b343c2ac6ab8867e1b10731ad
Georgi Dimitrov <43094653+Wafrey@users.noreply.github.com>
2019-02-10 20:50:07 +0200

    Add files via upload

---
+++ Arrays - More Exercise/03. Recursive Fibonacci/03. Recursive Fibonacci/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace _03._Recursive_Fibonacci
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int fibonacciNumber = int.Parse(Console.ReadLine());
+            int[] fibonacciNumbers = new int[50];
+            fibonacciNumbers[0] = 1;
+            fibonacciNumbers[1] = 1;
+
+            for (int i = 2; i <= fibonacciNumber; i++)
+            {
+                fibonacciNumbers[i] = fibonacciNumbers[i - 1] + fibonacciNumbers[i - 2];
+            }
+
+            Console.WriteLine(fibonacciNumbers[fibonacciNumber - 1]);
+
+
+        }
+    }
+}
