739369998615da43ab6c7681c86c15ede5eb88bc
Georgi Dimitrov <43094653+Wafrey@users.noreply.github.com>
2019-02-12 12:05:12 +0200

    Add files via upload

---
+++ Arrays - Exercise/Common Elements/Common Elements/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace Common_Elements
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var firstTexts = Console.ReadLine().Split();
+            var secondTexts = Console.ReadLine().Split();
+            string sameText = "";
+
+            for (int i = 0; i < secondTexts.Length; i++)
+            {
+                for (int k = 0; k < firstTexts.Length; k++)
+                {
+                    if (secondTexts[i] == firstTexts[k])
+                    {
+                        sameText += secondTexts[i] + " ";
+                    }
+                }
+            }
+
+            Console.WriteLine(sameText);
+        }
+    }
+}
