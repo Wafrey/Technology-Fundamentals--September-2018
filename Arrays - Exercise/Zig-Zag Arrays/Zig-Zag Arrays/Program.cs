5165543d8ebfb9f8545e7d56d1e7cfcc0d6cf5ba
Georgi Dimitrov <43094653+Wafrey@users.noreply.github.com>
2019-02-12 12:06:28 +0200

    Add files via upload

---
+++ Arrays - Exercise/Zig-Zag Arrays/Zig-Zag Arrays/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Linq;
+
+namespace Zig_Zag_Arrays
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            var firstResult = new int[n];
+            var secondResult = new int[n];
+
+            for (int i = 0; i < n; i++)
+            {
+                var currentArray = Console.ReadLine().Split().Select(int.Parse).ToArray();
+                if (i % 2 == 0)
+                {
+                    firstResult[i] = currentArray[0];
+                    secondResult[i] = currentArray[1];
+                }
+                else
+                {
+                    firstResult[i] = currentArray[1];
+                    secondResult[i] = currentArray[0];
+                }
+            }
+            Console.WriteLine(string.Join(' ', firstResult));
+            Console.WriteLine(string.Join(' ', secondResult));
+        }
+    }
+}
