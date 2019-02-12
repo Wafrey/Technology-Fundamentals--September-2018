bd3c695097aedf355335e17718702e284dbf2c19
Georgi Dimitrov <43094653+Wafrey@users.noreply.github.com>
2019-02-12 12:06:02 +0200

    Add files via upload

---
+++ Arrays - Exercise/Train/Train/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Train
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int countOfWagon = int.Parse(Console.ReadLine());
+            var peopleOnEachWagon = new int[countOfWagon];
+            int sumOfPeople = 0;
+
+            for (int i = 0; i < countOfWagon; i++)
+            {
+                peopleOnEachWagon[i] = int.Parse(Console.ReadLine());
+                sumOfPeople += peopleOnEachWagon[i];
+            }
+
+            Console.WriteLine(string.Join(' ', peopleOnEachWagon));
+            Console.WriteLine(sumOfPeople);
+        }
+    }
+}
