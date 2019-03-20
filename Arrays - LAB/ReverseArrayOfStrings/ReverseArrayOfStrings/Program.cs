b63afe764fdd5d257f548ad3eaf79e3e28af9a09
Georgi Dimitrov <43094653+Wafrey@users.noreply.github.com>
2019-03-20 18:04:49 +0200

    Add files via upload

---
+++ Arrays - LAB/ReverseArrayOfStrings/ReverseArrayOfStrings/Program.cs
@@ -0,0 +1,14 @@
+﻿namespace ReverseArrayOfStrings
+{
+    using System;
+    using System.Linq;
+
+    public class Program
+    {
+        public static void Main()
+        {
+            string[] symbols = Console.ReadLine().Split().ToArray();
+            Console.WriteLine(string.Join(" ", symbols.Reverse()));
+        }
+    }
+}
