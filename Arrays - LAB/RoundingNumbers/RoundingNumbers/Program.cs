dc2687a3a8f51814ee7edaf0321cd65a4739bcf8
Georgi Dimitrov <43094653+Wafrey@users.noreply.github.com>
2019-03-20 18:05:21 +0200

    Add files via upload

---
+++ Arrays - LAB/RoundingNumbers/RoundingNumbers/Program.cs
@@ -0,0 +1,19 @@
+﻿
+namespace RoundingNumbers
+{
+    using System;
+    using System.Linq;
+
+    public class Program
+    {
+        public static void Main()
+        {
+            double[] numbers = Console.ReadLine().Split().Select(double.Parse).ToArray();
+
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                Console.WriteLine($"{numbers[i]} => {numbers[i]:f0}");
+            }
+        }
+    }
+}
