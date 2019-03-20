0c57a2e8bd448eadc6c4c573275dba5d868e1333
Georgi Dimitrov <43094653+Wafrey@users.noreply.github.com>
2019-03-20 15:59:06 +0200

    Add files via upload

---
+++ Arrays - LAB/DayOfWeek/ConsoleApp1/Program.cs
@@ -0,0 +1,26 @@
+﻿namespace P01_DayOfWeek
+{
+    using System;
+
+    public class Program
+    {
+        public static void Main()
+        {
+            string[] days = new string[7] {"Monday", "Tuesday", "Wednesday", "Thursday",
+                "Friday", "Saturday", "Sunday" };
+
+            int day = int.Parse(Console.ReadLine());
+
+            if (day > 7 || day < 1)
+            {
+                Console.WriteLine("Invalid day!");
+            }
+
+            else
+            {
+                Console.WriteLine(days[day - 1]);
+            }
+        }
+    }
+}
+
---
+++ Arrays - LAB/PrintNumbersInReverseOrder/PrintNumbersInReverseOrder/Program.cs
@@ -0,0 +1,20 @@
+﻿namespace P02_PrintNumbersInReverseOrder
+{
+    using System;
+    using System.Linq;
+
+    public class Program
+    {
+        public static void Main()
+        {
+            int[] numbers = new int[int.Parse(Console.ReadLine())];
+
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                numbers[i] = int.Parse(Console.ReadLine());
+            }
+
+            Console.WriteLine(string.Join(" ", numbers.Reverse()));
+        }
+    }
+}
