4d57acbd041e6aee8c5145e7fd8df41dcc6a6313
Georgi Dimitrov <43094653+Wafrey@users.noreply.github.com>
2019-03-21 11:17:54 +0200

    Add files via upload

---
+++ Arrays - LAB/EvenAndOddSubstraction/EvenAndOddSubstraction/Program.cs
@@ -0,0 +1,30 @@
+﻿namespace P06_EvenAndOddSubstraction
+{
+    using System;
+    using System.Linq;
+
+    public class Program
+    {
+        public static void Main()
+        {
+            int[] numbers = Console.ReadLine().Split().Select(int.Parse).ToArray();
+            int evenSum = 0;
+            int oddSum = 0;
+
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                if (numbers[i] % 2 == 0)
+                {
+                    evenSum += numbers[i];
+                }
+
+                else
+                {
+                    oddSum += numbers[i];
+                }
+            }
+
+            Console.WriteLine(evenSum -= oddSum);
+        }
+    }
+}
