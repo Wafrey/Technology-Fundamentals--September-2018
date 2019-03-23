f55764302934c8bccb33d23b561a5da066ac0011
Georgi Dimitrov <43094653+Wafrey@users.noreply.github.com>
2019-03-23 13:21:54 +0200

    Add files via upload

---
+++ Basic Syntax, Conditional Statements And Loops - LAB/P03-PassedOrFailed/P03-PassedOrFailed/Program.cs
@@ -0,0 +1,22 @@
+﻿
+namespace P03_PassedOrFailed
+{
+    using System;
+
+    public class Program
+    {
+        public static void Main()
+        {
+            double grade = double.Parse(Console.ReadLine());
+            if (grade >= 3.00)
+            {
+                Console.WriteLine("Passed!");
+            }
+
+            else
+            {
+                Console.WriteLine("Failed!");
+            }
+        }
+    }
+}
