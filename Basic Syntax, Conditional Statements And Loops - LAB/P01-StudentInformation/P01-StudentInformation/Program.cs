65eb3ae317f3286532145e44ea8eaef853a5c7db
Georgi Dimitrov <43094653+Wafrey@users.noreply.github.com>
2019-03-23 13:21:22 +0200

    Add files via upload

---
+++ Basic Syntax, Conditional Statements And Loops - LAB/P01-StudentInformation/P01-StudentInformation/Program.cs
@@ -0,0 +1,16 @@
+﻿
+namespace P01_StudentInformation
+{
+    using System;
+
+    public class Program
+    {
+        public static void Main()
+        {
+            string name = Console.ReadLine();
+            int age = int.Parse(Console.ReadLine());
+            double grade = double.Parse(Console.ReadLine());
+            Console.WriteLine($"Name: {name}, Age: {age}, Grade: {grade:F2}");
+        }
+    }
+}
---
+++ Basic Syntax, Conditional Statements And Loops - LAB/P02-Passed/P02-Passed/Program.cs
@@ -0,0 +1,17 @@
+﻿
+namespace P02_Passed
+{
+    using System;
+
+    public class Program
+    {
+        public static void Main()
+        {
+            double grade = double.Parse(Console.ReadLine());
+            if (grade >= 3.00)
+            {
+                Console.WriteLine("Passed!");
+            }
+        }
+    }
+}
