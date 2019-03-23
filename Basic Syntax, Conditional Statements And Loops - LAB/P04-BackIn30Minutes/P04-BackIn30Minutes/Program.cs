01e146412288e5cf744300df3198f1daed690ec4
Georgi Dimitrov <43094653+Wafrey@users.noreply.github.com>
2019-03-23 13:22:24 +0200

    Add files via upload

---
+++ Basic Syntax, Conditional Statements And Loops - LAB/P04-BackIn30Minutes/P04-BackIn30Minutes/Program.cs
@@ -0,0 +1,27 @@
+﻿
+namespace P04_BackIn30Minutes
+{
+    using System;
+
+    public class Program
+    {
+        public static void Main()
+        {
+            int hours = int.Parse(Console.ReadLine());
+            int minutes = int.Parse(Console.ReadLine());
+            minutes += 30;
+
+            if (minutes > 59)
+            {
+                minutes -= 60;
+                hours += 1;
+                if (hours == 24)
+                {
+                    hours = 0;
+                }
+            }
+
+            Console.WriteLine($"{hours}:{minutes:D2}");
+        }
+    }
+}
