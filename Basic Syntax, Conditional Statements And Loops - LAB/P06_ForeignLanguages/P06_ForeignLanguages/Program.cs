236bee07fd4ef0c6dc889e694ee98b140c92e1fa
Georgi Dimitrov <43094653+Wafrey@users.noreply.github.com>
2019-03-23 13:23:28 +0200

    Add files via upload

---
+++ Basic Syntax, Conditional Statements And Loops - LAB/P06_ForeignLanguages/P06_ForeignLanguages/Program.cs
@@ -0,0 +1,29 @@
+﻿namespace P06_ForeignLanguages
+{
+    using System;
+
+    public class Program
+    {
+        public static void Main()
+        {
+            string country = Console.ReadLine();
+            switch (country)
+            {
+                case "USA":
+                case "England":
+                    Console.WriteLine("English");
+                    break;
+
+                case "Spain":
+                case "Argentina":
+                case "Mexico":
+                    Console.WriteLine("Spanish");
+                    break;
+
+                default:
+                    Console.WriteLine("unknown");
+                    break;
+            }
+        }
+    }
+}
