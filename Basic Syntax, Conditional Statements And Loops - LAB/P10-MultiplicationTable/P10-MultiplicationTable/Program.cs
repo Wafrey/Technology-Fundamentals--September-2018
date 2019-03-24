b2b420b02139b7184fa41116f089bd63f657a5e5
Georgi Dimitrov <43094653+Wafrey@users.noreply.github.com>
2019-03-24 14:27:49 +0200

    Add files via upload

---
+++ Basic Syntax, Conditional Statements And Loops - LAB/P10-MultiplicationTable/P10-MultiplicationTable/Program.cs
@@ -0,0 +1,16 @@
+﻿namespace P10_MultiplicationTable
+{
+    using System;
+
+    public class Program
+    {
+        public static void Main()
+        {
+            int number = int.Parse(Console.ReadLine());
+            for (int i = 1; i <= 10; i++)
+            {
+                Console.WriteLine($"{number} X {i} = {number * i}");
+            }
+        }
+    }
+}
