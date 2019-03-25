88bcb5dc46796844bf77d655574e66bf0fd37071
Georgi Dimitrov <43094653+Wafrey@users.noreply.github.com>
2019-03-25 12:11:23 +0200

    Add files via upload

---
+++ Basic Syntax, Conditional Statements And Loops - Exercise/P01-Ages/P01-Ages/obj/Debug/netcoreapp2.1/P04-PrintAndSum/P04-PrintAndSum/Program.cs
@@ -0,0 +1,23 @@
+﻿namespace P04_PrintAndSum
+{
+    using System;
+
+    public class Program
+    {
+        public static void Main()
+        {
+            int start = int.Parse(Console.ReadLine());
+            int end = int.Parse(Console.ReadLine());
+            int sum = 0;
+
+            for (int i = start; i <= end; i++)
+            {
+                Console.Write(i + " ");
+                sum += i;
+            }
+
+            Console.WriteLine();
+            Console.WriteLine($"Sum: {sum}");
+        }
+    }
+}
