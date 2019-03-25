1d2224c15360369265bbc8504bf1da03b716cc50
Georgi Dimitrov <43094653+Wafrey@users.noreply.github.com>
2019-03-25 12:14:01 +0200

    Add files via upload

---
+++ Basic Syntax, Conditional Statements And Loops - Exercise/P05-Login/P05_Login/Program.cs
@@ -0,0 +1,45 @@
+﻿namespace P05_Login
+{
+    using System;
+
+    public class Program
+    {
+        public static void Main()
+        {
+            string username = Console.ReadLine();
+            char[] cArray = username.ToCharArray();
+            string password = "";
+
+            for (int i = cArray.Length - 1; i >= 0; i--)
+            {
+                password += cArray[i];
+            }
+
+            string login = "";
+            int counter = 0;
+
+            while (login != password)
+            {
+                login = Console.ReadLine();
+                counter++;
+
+                if (login != password)
+                {
+                    if (counter == 4)
+                    {
+                        Console.WriteLine($"User {username} blocked!");
+                        break;
+                    }
+
+                    Console.WriteLine("Incorrect password. Try again.");
+                }
+
+                else if (login == password)
+                {
+                    Console.WriteLine($"User {username} logged in.");
+                    break;
+                }
+            }
+        }
+    }
+}
