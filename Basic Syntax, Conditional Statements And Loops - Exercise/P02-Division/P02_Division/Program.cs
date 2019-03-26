0738966ec3eb71b1a3615f6a2df57e89e6ec4a04
Georgi Dimitrov <43094653+Wafrey@users.noreply.github.com>
2019-03-26 17:50:30 +0200

    Add files via upload

---
+++ Basic Syntax, Conditional Statements And Loops - Exercise/P02-Division/P02_Division/Program.cs
@@ -0,0 +1,52 @@
+﻿
+namespace P02_Division
+{
+    using System;
+
+    public class Program
+    {
+        public static void Main()
+        {
+            int number = int.Parse(Console.ReadLine());
+            int division = 0;
+
+            if (number % 2 == 0 || number % 3 == 0 || number % 7 == 0)
+            {
+                if (number % 10 == 0)
+                {
+                    division = 10;
+                }
+
+                else if (number % 7 == 0)
+                {
+                    division = 7;
+                }
+
+                else if (number % 3 == 0)
+                {
+                    if ((number % 3 == 0 && number % 2 == 0) || number % 6 == 0)
+                    {
+                        division = 6;
+                    }
+
+                    else if (number % 3 == 0)
+                    {
+                        division = 3;
+                    }
+                }
+
+                else if (number % 2 == 0)
+                {
+                    division = 2;
+                }
+
+                Console.WriteLine($"The number is divisible by {division}");
+            }
+
+            else
+            {
+                Console.WriteLine("Not divisible");
+            }
+        }
+    }
+}
