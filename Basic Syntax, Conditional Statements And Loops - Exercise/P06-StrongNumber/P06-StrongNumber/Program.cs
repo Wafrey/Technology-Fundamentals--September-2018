7b17cd54f02cc8b58a271b10d0f6e8f7d4837860
Georgi Dimitrov <43094653+Wafrey@users.noreply.github.com>
2019-03-26 17:49:30 +0200

    Add files via upload

---
+++ Basic Syntax, Conditional Statements And Loops - Exercise/P06-StrongNumber/P06-StrongNumber/Program.cs
@@ -0,0 +1,56 @@
+﻿namespace P06_StrongNumber
+{
+    using System;
+
+    public class Program
+    {
+        public static void Main()
+        {
+            string number = Console.ReadLine();
+            char[] cArray = number.ToCharArray();
+            char digit = ' ';
+            int numDigit = 0;
+            int counter = 0;
+            int sum = 0;
+
+            for (int i = 0; i <= cArray.Length - 1; i++)
+            {
+                digit = cArray[i];
+                numDigit = (int)Char.GetNumericValue(digit);
+
+                if (numDigit == 1)
+                {
+                    sum += 1;
+                    continue;
+                }
+
+                else if (numDigit == 0)
+                {
+                    sum += 1;
+                    continue;
+                }
+
+                counter = numDigit;
+
+                for (int j = 1; j < counter; j++)
+                {
+                    numDigit = numDigit * j;
+                }
+
+                sum += numDigit;
+            }
+
+            int num = Convert.ToInt32(number);
+
+            if (sum == num)
+            {
+                Console.WriteLine("yes");
+            }
+
+            else
+            {
+                Console.WriteLine("no");
+            }
+        }
+    }
+}
