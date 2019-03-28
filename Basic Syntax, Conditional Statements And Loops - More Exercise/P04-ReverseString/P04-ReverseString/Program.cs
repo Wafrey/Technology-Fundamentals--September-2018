27c620bf3c15bc9b2881fbee87f7bd183b2209db
Georgi Dimitrov <43094653+Wafrey@users.noreply.github.com>
2019-03-28 15:51:31 +0200

    Add files via upload

---
+++ Basic Syntax, Conditional Statements And Loops - More Exercise/P04-ReverseString/P04-ReverseString/Program.cs
@@ -0,0 +1,21 @@
+﻿namespace P04_ReverseString
+{
+    using System;
+
+    public class Program
+    {
+        public static void Main()
+        {
+            string input = Console.ReadLine();
+            char[] cArray = input.ToCharArray();
+            string reversedInput = "";
+
+            for (int i = cArray.Length - 1; i >= 0; i--)
+            {
+                reversedInput += cArray[i];
+            }
+
+            Console.WriteLine(reversedInput);
+        }
+    }
+}
