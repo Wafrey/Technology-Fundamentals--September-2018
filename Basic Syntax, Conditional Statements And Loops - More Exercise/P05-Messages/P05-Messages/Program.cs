07c3272f83cc9e54dbf28b2584bc829356d7eb73
Georgi Dimitrov <43094653+Wafrey@users.noreply.github.com>
2019-03-28 15:52:01 +0200

    Add files via upload

---
+++ Basic Syntax, Conditional Statements And Loops - More Exercise/P05-Messages/P05-Messages/Program.cs
@@ -0,0 +1,67 @@
+﻿
+namespace P05_Messages
+{
+    using System;
+
+    public class Program
+    {
+        public static void Main()
+        {
+            int number = int.Parse(Console.ReadLine());
+            string message = string.Empty;
+            int digitLength = 0;
+            int mainDigit = 0;
+            int offset = 0;
+            int letterIndex = 0;
+            char letter = ' ';
+
+            for (int i = 0; i < number; i++)
+            {
+                int input = int.Parse(Console.ReadLine());
+
+                if (input / 1000 > 0)
+                {
+                    digitLength = 4;
+                    mainDigit = input / 1000;
+                }
+
+                else if (input / 100 > 0)
+                {
+                    digitLength = 3;
+                    mainDigit = input / 100;
+                }
+
+                else if (input / 10 > 0)
+                {
+                    digitLength = 2;
+                    mainDigit = input / 10;
+                }
+
+                else
+                {
+                    digitLength = 1;
+                    mainDigit = input;
+                }
+
+                offset = (mainDigit - 2) * 3;
+
+                if (mainDigit == 8 || mainDigit == 9)
+                {
+                    offset += 1;
+                }
+
+                letterIndex = offset + digitLength - 1;
+                letter = (char)(letterIndex + 97);
+
+                if (input == 0)
+                {
+                    letter = ' ';
+                }
+
+                message += letter;
+            }
+
+            Console.WriteLine(message);
+        }
+    }
+}
