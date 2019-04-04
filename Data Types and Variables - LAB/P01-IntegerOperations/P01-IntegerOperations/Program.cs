7d2d1d71f92cc2660e4aecfacc86cfc6f75ce45e
Georgi Dimitrov <43094653+Wafrey@users.noreply.github.com>
2019-04-04 18:40:48 +0300

    Add files via upload

---
+++ Data Types and Variables - LAB/P01-IntegerOperations/P01-IntegerOperations/Program.cs
@@ -0,0 +1,21 @@
+﻿namespace P01_IntegerOperations
+{
+    using System;
+
+    public class Program
+    {
+        public static void Main()
+        {
+            long firstNumber = long.Parse(Console.ReadLine());
+            int secondNumber = int.Parse(Console.ReadLine());
+            int thirdNumber = int.Parse(Console.ReadLine());
+            int fourthNumber = int.Parse(Console.ReadLine());
+
+            firstNumber += secondNumber;
+            firstNumber /= thirdNumber;
+            firstNumber *= fourthNumber;
+
+            Console.WriteLine(firstNumber);
+        }
+    }
+}
---
+++ Data Types and Variables - LAB/P02-CircleArea/P02-CircleArea/Program.cs
@@ -0,0 +1,14 @@
+﻿namespace P02_CircleArea
+{
+    using System;
+
+    public class Program
+    {
+        public static void Main()
+        {
+            double rarius = double.Parse(Console.ReadLine());
+            double area = Math.PI * rarius * rarius;
+            Console.WriteLine($"{area:f12}");
+        }
+    }
+}
