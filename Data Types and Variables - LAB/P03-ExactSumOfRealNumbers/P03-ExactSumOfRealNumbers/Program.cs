1e62682c0265af8542a79c025e0ee41fd08ed898
Georgi Dimitrov <43094653+Wafrey@users.noreply.github.com>
2019-04-04 18:41:27 +0300

    Add files via upload

---
+++ Data Types and Variables - LAB/P03-ExactSumOfRealNumbers/P03-ExactSumOfRealNumbers/Program.cs
@@ -0,0 +1,21 @@
+﻿namespace P03_ExactSumOfRealNumbers
+{
+    using System;
+
+    public class Program
+    {
+        public static void Main()
+        {
+            int n = int.Parse(Console.ReadLine());
+            decimal sum = 0m;
+
+            for (int i = 0; i < n; i++)
+            {
+                decimal number = decimal.Parse(Console.ReadLine());
+                sum += number;
+            }
+
+            Console.WriteLine(sum);
+        }
+    }
+}
