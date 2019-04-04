f27c61cdf5502770860330e4051715f8f1341289
Georgi Dimitrov <43094653+Wafrey@users.noreply.github.com>
2019-04-04 18:42:02 +0300

    Add files via upload

---
+++ Data Types and Variables - LAB/P04-Elevator/P04-Elevator/Program.cs
@@ -0,0 +1,26 @@
+﻿namespace P04_Elevator
+{
+    using System;
+
+    public class Program
+    {
+        public static void Main()
+        {
+            int people = int.Parse(Console.ReadLine());
+            int capacity = int.Parse(Console.ReadLine());
+            int courses = 0;
+
+            if (people % capacity == 0)
+            {
+                courses = people / capacity;
+            }
+
+            else
+            {
+                courses = (people / capacity) + 1;
+            }
+
+            Console.WriteLine(courses);
+        }
+    }
+}
