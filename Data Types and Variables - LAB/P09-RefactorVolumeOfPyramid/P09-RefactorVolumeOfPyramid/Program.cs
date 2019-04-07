a1dfe172a567d839ca1cf9d5afb737ba9b2dbf29
Georgi Dimitrov <43094653+Wafrey@users.noreply.github.com>
2019-04-07 13:32:57 +0300

    Add files via upload

---
+++ Data Types and Variables - LAB/P09-RefactorVolumeOfPyramid/P09-RefactorVolumeOfPyramid/Program.cs
@@ -0,0 +1,22 @@
+﻿namespace P09_RefactorVolumeOfPyramid
+{
+    using System;
+
+    public class Program
+    {
+        public static void Main()
+        {
+            Console.Write("Length: ");
+            double length = double.Parse(Console.ReadLine());
+
+            Console.Write("Width: ");
+            double width = double.Parse(Console.ReadLine());
+
+            Console.Write("Height: ");
+            double height = double.Parse(Console.ReadLine());
+
+            double volume = (length * width * height) / 3;
+            Console.WriteLine($"Pyramid Volume: {volume:F2}");
+        }
+    }
+}
