4da99e61456db42e3ac3a2bba91e962f6beb0bfc
Georgi Dimitrov <43094653+Wafrey@users.noreply.github.com>
2019-04-07 13:32:05 +0300

    Add files via upload

---
+++ Data Types and Variables - LAB/P07-TriplesOfLatinLetters/P07-TriplesOfLatinLetters/Program.cs
@@ -0,0 +1,25 @@
+﻿namespace P07_TriplesOfLatinLetters
+{
+    using System;
+
+    public class Program
+    {
+        public static void Main()
+        {
+            int n = int.Parse(Console.ReadLine());
+            char letter = (char)('a' + n);
+
+            for (char i = 'a'; i < letter; i++)
+            {
+                for (char j = 'a'; j < letter; j++)
+                {
+                    for (char k = 'a'; k < letter; k++)
+                    {
+                        Console.Write($"{i}{j}{k}");
+                        Console.WriteLine();
+                    }
+                }
+            }
+        }
+    }
+}
