558e427f7867da6647579bf2184b71a596dcd77b
Georgi Dimitrov <43094653+Wafrey@users.noreply.github.com>
2019-04-07 13:33:24 +0300

    Add files via upload

---
+++ Data Types and Variables - LAB/P10-RefactorSpecialNumbers/P10-RefactorSpecialNumbers/Program.cs
@@ -0,0 +1,21 @@
+﻿namespace P10_RefactorSpecialNumbers
+{
+    using System;
+
+    public class Program
+    {
+        public static void Main()
+        {
+            int num = int.Parse(Console.ReadLine());
+            int sum = 0; bool special = false;
+
+            for (int i = 1; i <= num; i++)
+            {
+                sum = (i % 10) + (i / 10);
+                special = (sum == 5) || (sum == 7) || (sum == 11);
+                Console.WriteLine($"{i} -> {special}");
+                sum = 0;
+            }
+        }
+    }
+}
