44f9f4c9660bdbe943feb65036abfb1293ef49b0
Georgi Dimitrov <43094653+Wafrey@users.noreply.github.com>
2019-04-07 13:32:31 +0300

    Add files via upload

---
+++ Data Types and Variables - LAB/P08-ConcatNames/P08-ConcatNames/Program.cs
@@ -0,0 +1,16 @@
+﻿namespace P08_ConcatNames
+{
+    using System;
+
+    public class Program
+    {
+        public static void Main()
+        {
+            string firstName = Console.ReadLine();
+            string lastName = Console.ReadLine();
+            string delimiter = Console.ReadLine();
+
+            Console.WriteLine($"{firstName}{delimiter}{lastName}");
+        }
+    }
+}
