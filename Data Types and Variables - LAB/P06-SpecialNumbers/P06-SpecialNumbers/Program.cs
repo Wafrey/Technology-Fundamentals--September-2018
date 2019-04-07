793c78d7c5a439aa0c160afdd5c4a21962285bcc
Georgi Dimitrov <43094653+Wafrey@users.noreply.github.com>
2019-04-07 13:31:39 +0300

    Add files via upload

---
+++ Data Types and Variables - LAB/P06-SpecialNumbers/P06-SpecialNumbers/Program.cs
@@ -0,0 +1,22 @@
+﻿namespace P06_SpecialNumbers
+{
+    using System;
+
+    public class Program
+    {
+        public static void Main()
+        {
+            int num = int.Parse(Console.ReadLine());
+            int sum = 0;
+            bool special = false;
+
+            for (int i = 1; i <= num; i++)
+            {
+                sum = (i / 10) + (i % 10);
+                special = (sum == 5 || sum == 7 || sum == 11);
+                Console.WriteLine($"{i} -> {special}");
+                sum = 0;
+            }
+        }
+    }
+}
