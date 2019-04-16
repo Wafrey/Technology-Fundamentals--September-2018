8f0e8bee040c5b32a10ae3949b4d64a934af07f9
Georgi Dimitrov <43094653+Wafrey@users.noreply.github.com>
2019-04-16 18:08:45 +0300

    Add files via upload

---
+++ Data Types and Variables - Exercise/P01-SumDigits/P01-SumDigits/Program.cs
@@ -0,0 +1,21 @@
+﻿namespace P01_SumDigits
+{
+    using System;
+
+    public class Program
+    {
+        public static void Main()
+        {
+            int number = int.Parse(Console.ReadLine());
+            int sum = 0;
+
+            while (number > 0)
+            {
+                sum += number % 10;
+                number /= 10;
+            }
+
+            Console.WriteLine(sum);
+        }
+    }
+}
---
+++ Data Types and Variables - Exercise/P02-CharsToString/P02-CharsToString/Program.cs
@@ -0,0 +1,18 @@
+﻿namespace P02_CharsToString
+{
+    using System;
+
+    public class Program
+    {
+        public static void Main()
+        {
+            char firstInput = char.Parse(Console.ReadLine());
+            char secondInput = char.Parse(Console.ReadLine());
+            char thirdInput = char.Parse(Console.ReadLine());
+            string text = string.Empty;
+
+            text = text + firstInput + secondInput + thirdInput;
+            Console.WriteLine(text);
+        }
+    }
+}
