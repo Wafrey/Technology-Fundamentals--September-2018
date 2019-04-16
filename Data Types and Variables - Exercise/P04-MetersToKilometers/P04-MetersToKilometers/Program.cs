6d6a81bfd0a80984d15d49ceabee980efc09bddc
Georgi Dimitrov <43094653+Wafrey@users.noreply.github.com>
2019-04-16 18:10:06 +0300

    Add files via upload

---
+++ Data Types and Variables - Exercise/P04-MetersToKilometers/P04-MetersToKilometers/Program.cs
@@ -0,0 +1,14 @@
+﻿namespace P04_MetersToKilometers
+{
+    using System;
+
+    public class Program
+    {
+        public static void Main()
+        {
+            int meters = int.Parse(Console.ReadLine());
+            double km = meters / 1000.0;
+            Console.WriteLine($"{km:f2}");
+        }
+    }
+}
