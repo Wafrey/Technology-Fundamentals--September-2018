d4c0977a46600ac95cf2b4ae0708b69db4365d0d
Georgi Dimitrov <43094653+Wafrey@users.noreply.github.com>
2019-04-16 18:09:10 +0300

    Add files via upload

---
+++ Data Types and Variables - Exercise/P03-TownInfo/P03-TownInfo/Program.cs
@@ -0,0 +1,16 @@
+﻿namespace P03_TownInfo
+{
+    using System;
+
+    public class Program
+    {
+        public static void Main()
+        {
+            string town = Console.ReadLine();
+            int population = int.Parse(Console.ReadLine());
+            int area = int.Parse(Console.ReadLine());
+
+            Console.WriteLine($"Town {town} has population of {population} and area {area} square km.");
+        }
+    }
+}
