896bca645c64af7e5ff0bc1e5af152c8596fc4cf
Georgi Dimitrov <43094653+Wafrey@users.noreply.github.com>
2019-04-16 18:10:34 +0300

    Add files via upload

---
+++ Data Types and Variables - Exercise/P05-PoundsToDollars/P05-PoundsToDollars/Program.cs
@@ -0,0 +1,14 @@
+﻿namespace P05_PoundsToDollars
+{
+    using System;
+
+    public class Program
+    {
+        public static void Main()
+        {
+            decimal pounds = decimal.Parse(Console.ReadLine());
+            decimal dollars = pounds * 1.31m;
+            Console.WriteLine($"{dollars:f3}");
+        }
+    }
+}
