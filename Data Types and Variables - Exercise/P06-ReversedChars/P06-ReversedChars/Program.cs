4f1de035bee79376b46ad80d8653bb26d7e3e9e6
Georgi Dimitrov <43094653+Wafrey@users.noreply.github.com>
2019-04-17 12:34:00 +0300

    Add files via upload

---
+++ Data Types and Variables - Exercise/P06-ReversedChars/P06-ReversedChars/Program.cs
@@ -0,0 +1,18 @@
+﻿namespace P06_ReversedChars
+{
+    using System;
+
+    public class Program
+    {
+        public static void Main()
+        {
+            char firstInput = char.Parse(Console.ReadLine());
+            char secondInput = char.Parse(Console.ReadLine());
+            char thirdInput = char.Parse(Console.ReadLine());
+            string text = string.Empty;
+
+            text = text + thirdInput + " " + secondInput + " " + firstInput;
+            Console.WriteLine(text);
+        }
+    }
+}
