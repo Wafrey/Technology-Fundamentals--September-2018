7bae9775192fcfcf30468046f08efe86f7486ae6
Georgi Dimitrov <43094653+Wafrey@users.noreply.github.com>
2019-04-17 12:34:49 +0300

    Add files via upload

---
+++ Data Types and Variables - Exercise/P07-LowerOrUpper/P07-LowerOrUpper/Program.cs
@@ -0,0 +1,22 @@
+﻿namespace P07_LowerOrUpper
+{
+    using System;
+
+    public class Program
+    {
+        public static void Main()
+        {
+            char letter = char.Parse(Console.ReadLine());
+
+            if (letter >= 65 && letter <= 90)
+            {
+                Console.WriteLine("upper-case");
+            }
+
+            else if (letter >= 97 && letter <= 122)
+            {
+                Console.WriteLine("lower-case");
+            }
+        }
+    }
+}
