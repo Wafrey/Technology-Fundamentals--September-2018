cb82feaaadf0c2ce05756ae9565e27c3171e8fd9
Georgi Dimitrov <43094653+Wafrey@users.noreply.github.com>
2019-04-17 12:36:12 +0300

    Add files via upload

---
+++ Data Types and Variables - Exercise/P09-PokeMon/P09-PokeMon/Program.cs
@@ -0,0 +1,33 @@
+﻿namespace P09_PokeMon
+{
+    using System;
+
+    public class Program
+    {
+        public static void Main()
+        {
+            long pokePower = long.Parse(Console.ReadLine());
+            decimal halfPower = pokePower / 2.0m;
+            int distance = int.Parse(Console.ReadLine());
+            int exhaustionFactor = int.Parse(Console.ReadLine());
+            int targetCount = 0;
+
+            while (pokePower >= distance)
+            {
+                pokePower -= distance;
+                targetCount++;
+
+                if (pokePower == halfPower)
+                {
+                    if (exhaustionFactor > 0)
+                    {
+                        pokePower /= exhaustionFactor;
+                    }
+                }
+            }
+
+            Console.WriteLine(pokePower);
+            Console.WriteLine(targetCount);
+        }
+    }
+}
