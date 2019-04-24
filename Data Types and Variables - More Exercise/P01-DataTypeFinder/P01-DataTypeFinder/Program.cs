a6e35e74a2c2dd19fc99e5143f6d606d9d5d2e4c
Georgi Dimitrov <43094653+Wafrey@users.noreply.github.com>
2019-04-24 13:07:15 +0300

    Add files via upload

---
+++ Data Types and Variables - More Exercise/P01-DataTypeFinder/P01-DataTypeFinder/Program.cs
@@ -0,0 +1,49 @@
+﻿namespace P01_DataTypeFinder
+{
+    using System;
+
+    public class Program
+    {
+        public static void Main()
+        {
+            string input = Console.ReadLine(); ;
+            string dataType = string.Empty;
+
+            while (input != "END")
+            {
+                if (input == "END")
+                {
+                    break;
+                }
+
+                else if (int.TryParse(input, out int result))
+                {
+                    dataType = "integer";
+                }
+
+                else if (double.TryParse(input, out double result1))
+                {
+                    dataType = "floating point";
+                }
+
+                else if (bool.TryParse(input, out bool result2))
+                {
+                    dataType = "boolean";
+                }
+
+                else if (char.TryParse(input, out char result3))
+                {
+                    dataType = "character";
+                }
+
+                else
+                {
+                    dataType = "string";
+                }
+
+                Console.WriteLine($"{input} is {dataType} type");
+                input = Console.ReadLine();
+            }
+        }
+    }
+}
