65d210fa9ef9d5b42747db307ce7e6c93972cec6
Georgi Dimitrov <43094653+Wafrey@users.noreply.github.com>
2019-04-24 13:09:41 +0300

    Add files via upload

---
+++ Data Types and Variables - More Exercise/P03-ExchangeIntegers/P03-ExchangeIntegers/Program.cs
@@ -0,0 +1,25 @@
+﻿namespace P03_ExchangeIntegers
+{
+    using System;
+
+    public class Program
+    {
+        public static void Main()
+        {
+            int firstNumber = int.Parse(Console.ReadLine());
+            int secondNumber = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Before:");
+            Console.WriteLine("a = " + firstNumber);
+            Console.WriteLine("b = " + secondNumber);
+
+            int temporaryNumber = firstNumber;
+            firstNumber = secondNumber;
+            secondNumber = temporaryNumber;
+
+            Console.WriteLine("After:");
+            Console.WriteLine("a = " + firstNumber);
+            Console.WriteLine("b = " + secondNumber);
+        }
+    }
+}
