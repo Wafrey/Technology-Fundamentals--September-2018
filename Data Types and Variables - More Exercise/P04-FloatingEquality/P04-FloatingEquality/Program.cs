9b0b4ae38fcec906fb1d9372dfa427a2173ebb11
Georgi Dimitrov <43094653+Wafrey@users.noreply.github.com>
2019-04-24 13:13:12 +0300

    Add files via upload

---
+++ Data Types and Variables - More Exercise/P04-FloatingEquality/P04-FloatingEquality/Program.cs
@@ -0,0 +1,24 @@
+﻿namespace P04_FloatingEquality
+{
+    using System;
+
+    public class Program
+    {
+        public static void Main()
+        {
+            decimal firstNumber = decimal.Parse(Console.ReadLine());
+            decimal secondNumber = decimal.Parse(Console.ReadLine());
+            decimal diff = Math.Abs(firstNumber - secondNumber);
+
+            if (diff < 0.000001m)
+            {
+                Console.WriteLine("True");
+            }
+
+            else
+            {
+                Console.WriteLine("False");
+            }
+        }
+    }
+}
