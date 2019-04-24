41784cf3e809b94e76da68a61fa9291bd61b7a4a
Georgi Dimitrov <43094653+Wafrey@users.noreply.github.com>
2019-04-24 13:15:02 +0300

    Add files via upload

---
+++ Data Types and Variables - More Exercise/P05-RefactoringPrimeChecker/P05-RefactoringPrimeChecker/Program.cs
@@ -0,0 +1,28 @@
+﻿namespace P05_RefactoringPrimeChecker
+{
+    using System;
+
+    public class Program
+    {
+        public static void Main()
+        {
+            int number = int.Parse(Console.ReadLine());
+
+            for (int i = 2; i <= number; i++)
+            {
+                bool prime = true;
+
+                for (int j = 2; j < i; j++)
+                {
+                    if (i % j == 0)
+                    {
+                        prime = false;
+                        break;
+                    }
+                }
+
+                Console.WriteLine($"{i} -> {prime.ToString().ToLower()}");
+            }
+        }
+    }
+}
