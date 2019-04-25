ed13ff67b5a1fb32e16021cd7b9174a2d949530f
Georgi Dimitrov <43094653+Wafrey@users.noreply.github.com>
2019-04-25 14:52:58 +0300

    Add files via upload

---
+++ Methods - LAB/P01-SignOgIntegerNumbers/P01-SignOgIntegerNumbers/Program.cs
@@ -0,0 +1,30 @@
+﻿namespace P01_SignOgIntegerNumbers
+{
+    using System;
+
+    public class Program
+    {
+        public static void Main()
+        {
+            PrintSign(int.Parse(Console.ReadLine()));
+        }
+
+        public static void PrintSign(int number)
+        {
+            if (number > 0)
+            {
+                Console.WriteLine($"The number {number} is positive.");
+            }
+
+            else if (number < 0)
+            {
+                Console.WriteLine($"The number {number} is negative.");
+            }
+
+            else
+            {
+                Console.WriteLine($"The number {number} is zero.");
+            }
+        }
+    }
+}
