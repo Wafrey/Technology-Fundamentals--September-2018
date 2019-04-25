edc19c4d0919f7212179d737e519cc6d5cc8696f
Georgi Dimitrov <43094653+Wafrey@users.noreply.github.com>
2019-04-25 14:54:52 +0300

    Add files via upload

---
+++ Methods - LAB/P05-Orders/P05-Orders/Program.cs
@@ -0,0 +1,35 @@
+﻿namespace P05_Orders
+{
+    using System;
+
+    public class Program
+    {
+        public static void Main()
+        {
+            CalculatePrice(Console.ReadLine(), int.Parse(Console.ReadLine()));
+        }
+
+        public static void CalculatePrice(string product, int quantity)
+        {
+            if (product == "coffee")
+            {
+                Console.WriteLine($"{(quantity * 1.50):F2}");
+            }
+
+            else if (product == "water")
+            {
+                Console.WriteLine($"{(quantity * 1.00):F2}");
+            }
+
+            else if (product == "coke")
+            {
+                Console.WriteLine($"{(quantity * 1.40):F2}");
+            }
+
+            else if (product == "snacks")
+            {
+                Console.WriteLine($"{(quantity * 2.00):F2}");
+            }
+        }
+    }
+}
