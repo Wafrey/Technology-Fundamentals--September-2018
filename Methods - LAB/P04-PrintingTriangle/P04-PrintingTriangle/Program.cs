1e7a63034e5b0fb719d5b44e4d047e189cce4ffe
Georgi Dimitrov <43094653+Wafrey@users.noreply.github.com>
2019-04-25 14:54:00 +0300

    Add files via upload

---
+++ Methods - LAB/P04-PrintingTriangle/P04-PrintingTriangle/Program.cs
@@ -0,0 +1,32 @@
+﻿namespace P04_PrintingTriangle
+{
+    using System;
+
+    public class Program
+    {
+        public static void Main()
+        {
+            int number = int.Parse(Console.ReadLine());
+
+            for (int i = 1; i <= number; i++)
+            {
+                PrintLine(1, i);
+            }
+
+            for (int i = number - 1; i >= 1; i--)
+            {
+                PrintLine(1, i);
+            }
+        }
+
+        public static void PrintLine(int start, int end)
+        {
+            for (int i = start; i <= end; i++)
+            {
+                Console.Write(i + " ");
+            }
+
+            Console.WriteLine();
+        }
+    }
+}
