682cf5033b1421690d5b2082daf1257b93e9ba9f
Georgi Dimitrov <43094653+Wafrey@users.noreply.github.com>
2019-04-25 14:55:19 +0300

    Add files via upload

---
+++ Methods - LAB/P06-CalculateRectangleArea/P06-CalculateRectangleArea/Program.cs
@@ -0,0 +1,18 @@
+﻿namespace P06_CalculateRectangleArea
+{
+    using System;
+
+    public class Program
+    {
+        public static void Main()
+        {
+            double area = GetRectangleArea(double.Parse(Console.ReadLine()), double.Parse(Console.ReadLine()));
+            Console.WriteLine(area);
+        }
+
+        public static double GetRectangleArea(double width, double height)
+        {
+            return width * height;
+        }
+    }
+}
