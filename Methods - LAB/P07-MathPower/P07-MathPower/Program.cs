57e6a16880d9a0b39a5af4a471f4e6050768541f
Georgi Dimitrov <43094653+Wafrey@users.noreply.github.com>
2019-04-25 14:56:24 +0300

    Add files via upload

---
+++ Methods - LAB/P07-MathPower/P07-MathPower/Program.cs
@@ -0,0 +1,18 @@
+﻿namespace P07_MathPower
+{
+    using System;
+
+    public class Program
+    {
+        public static void Main()
+        {
+            double finalResult = RaiseNumber(double.Parse(Console.ReadLine()), int.Parse(Console.ReadLine()));
+            Console.WriteLine(finalResult);
+        }
+
+        public static double RaiseNumber(double number, int power)
+        {
+            return Math.Pow(number, power);
+        }
+    }
+}
