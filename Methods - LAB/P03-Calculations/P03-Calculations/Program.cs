a6b07dd859fc76b52ea290f3cd27e4b24399b1c6
Georgi Dimitrov <43094653+Wafrey@users.noreply.github.com>
2019-04-25 14:53:31 +0300

    Add files via upload

---
+++ Methods - LAB/P03-Calculations/P03-Calculations/Program.cs
@@ -0,0 +1,54 @@
+﻿namespace P03_Calculations
+{
+    using System;
+
+    public class Program
+    {
+        public static void Main()
+        {
+            string command = Console.ReadLine();
+            double firstNumber = double.Parse(Console.ReadLine());
+            double secondNumber = double.Parse(Console.ReadLine());
+
+            if (command == "add")
+            {
+                AddNumbers(firstNumber, secondNumber);
+            }
+
+            else if (command == "multiply")
+            {
+                MultiplyNumbers(firstNumber, secondNumber);
+            }
+
+            else if (command == "substract")
+            {
+                SubstractNumbers(firstNumber, secondNumber);
+            }
+
+            else if (command == "divide")
+            {
+                DivideNumbers(firstNumber, secondNumber);
+            }
+        }
+
+        public static void AddNumbers(double firstNumber, double secondNumber)
+        {
+            Console.WriteLine(firstNumber + secondNumber);
+        }
+
+        public static void MultiplyNumbers(double firstNumber, double secondNumber)
+        {
+            Console.WriteLine(firstNumber * secondNumber);
+        }
+
+        public static void SubstractNumbers(double firstNumber, double secondNumber)
+        {
+            Console.WriteLine(firstNumber - secondNumber);
+        }
+
+        public static void DivideNumbers(double firstNumber, double secondNumber)
+        {
+            Console.WriteLine(firstNumber / secondNumber);
+        }
+    }
+}
