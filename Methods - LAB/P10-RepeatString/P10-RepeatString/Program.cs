0e6fda3a8b456dec97eba8379fad711ffca7acdc
Georgi Dimitrov <43094653+Wafrey@users.noreply.github.com>
2019-04-26 13:19:32 +0300

    Add files via upload

---
+++ Methods - LAB/P10-RepeatString/P10-RepeatString/Program.cs
@@ -0,0 +1,25 @@
+﻿namespace P10_RepeatString
+{
+    using System;
+
+    public class Program
+    {
+        public static void Main()
+        {
+            string result = RepeatString(Console.ReadLine(), int.Parse(Console.ReadLine()));
+            Console.WriteLine(result);
+        }
+
+        public static string RepeatString(string text, int count)
+        {
+            string repeatedText = string.Empty;
+
+            for (int i = 0; i < count; i++)
+            {
+                repeatedText += text;
+            }
+
+            return repeatedText;
+        }
+    }
+}
