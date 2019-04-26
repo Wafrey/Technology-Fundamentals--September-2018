f37e94d21d0dac4e2899426243eaa0378df158a5
Georgi Dimitrov <43094653+Wafrey@users.noreply.github.com>
2019-04-26 13:15:54 +0300

    Add files via upload

---
+++ Methods - LAB/P08-GreaterOfTwoValues/P08-GreaterOfTwoValues/Program.cs
@@ -0,0 +1,52 @@
+﻿namespace P08_GreaterOfTwoValues
+{
+    using System;
+
+    public class Program
+    {
+        public static void Main()
+        {
+            string valuesType = Console.ReadLine();
+
+            switch (valuesType)
+            {
+                case "int":
+                    GetMax(int.Parse(Console.ReadLine()), int.Parse(Console.ReadLine()));
+                    break;
+
+                case "char":
+                    GetMax(char.Parse(Console.ReadLine()), char.Parse(Console.ReadLine()));
+                    break;
+
+                case "string":
+                    GetMax(Console.ReadLine(), Console.ReadLine());
+                    break;
+            }
+        }
+
+        public static void GetMax(int firstNumber, int secondNumber)
+        {
+            Console.WriteLine(Math.Max(firstNumber, secondNumber));
+        }
+
+        public static void GetMax(char firstSymbol, char secondSymbol)
+        {
+            Console.WriteLine((char)Math.Max(firstSymbol, secondSymbol));
+        }
+
+        public static void GetMax(string firstText, string secondText)
+        {
+            int comparison = firstText.CompareTo(secondText);
+
+            if (comparison > 0)
+            {
+                Console.WriteLine(firstText);
+            }
+
+            else
+            {
+                Console.WriteLine(secondText);
+            }
+        }
+    }
+}
