f8d22a3b6ba78da03bacdd26daefb684bc7d70d3
Georgi Dimitrov <43094653+Wafrey@users.noreply.github.com>
2019-04-27 12:42:46 +0300

    Add files via upload

---
+++ Methods - Exercise/P05-AddAndSubstract/P05-AddAndSubstract/Program.cs
@@ -0,0 +1,29 @@
+﻿namespace P05_AddAndSubstract
+{
+    using System;
+
+    public class Program
+    {
+        public static void Main()
+        {
+            int firstNumber = int.Parse(Console.ReadLine());
+            int secondNumber = int.Parse(Console.ReadLine());
+            int thirdNumber = int.Parse(Console.ReadLine());
+
+            int sum = SumNumbers(firstNumber, secondNumber);
+            int substraction = SubstractNumbers(sum, thirdNumber);
+
+            Console.WriteLine(substraction);
+        }
+
+        public static int SumNumbers(int firstNumber, int secondNumber)
+        {
+            return firstNumber + secondNumber;
+        }
+
+        public static int SubstractNumbers(int sum, int thirdNumber)
+        {
+            return sum - thirdNumber;
+        }
+    }
+}
