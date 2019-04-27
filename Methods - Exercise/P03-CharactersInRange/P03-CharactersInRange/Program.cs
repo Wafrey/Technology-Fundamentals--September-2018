e9299e98dd71f70f70829aa6e471cbe4cbd9292f
Georgi Dimitrov <43094653+Wafrey@users.noreply.github.com>
2019-04-27 12:41:55 +0300

    Add files via upload

---
+++ Methods - Exercise/P03-CharactersInRange/P03-CharactersInRange/Program.cs
@@ -0,0 +1,32 @@
+﻿namespace P03_CharactersInRange
+{
+    using System;
+
+    public class Program
+    {
+        public static void Main()
+        {
+            char firstChar = char.Parse(Console.ReadLine());
+            char secondChar = char.Parse(Console.ReadLine());
+
+            char[] array = PrintChars(firstChar, secondChar);
+            Console.WriteLine(string.Join(" ", array));
+        }
+
+        public static char[] PrintChars(char firstChar, char secondChar)
+        {
+            char[] symbols = new char[Math.Abs(secondChar - firstChar) - 1];
+            int counter = 0;
+            int start = Math.Min(firstChar, secondChar);
+            int end = Math.Max(firstChar, secondChar);
+
+            for (int i = start + 1; i < end; i++)
+            {
+                symbols[counter] = (char)i;
+                counter++;
+            }
+
+            return symbols;
+        }
+    }
+}
