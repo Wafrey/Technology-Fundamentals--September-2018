56bc1564eaac939c55275b1ceadcab703212ac4e
Georgi Dimitrov <43094653+Wafrey@users.noreply.github.com>
2019-04-27 12:41:13 +0300

    Add files via upload

---
+++ Methods - Exercise/P01-SmallestOfThreeNumbers/P01-SmallestOfThreeNumbers/Program.cs
@@ -0,0 +1,24 @@
+﻿namespace P01_SmallestOfThreeNumbers
+{
+    using System;
+
+    public class Program
+    {
+        public static void Main()
+        {
+            int firstNumber = int.Parse(Console.ReadLine());
+            int secondNumber = int.Parse(Console.ReadLine());
+            int thirdNumber = int.Parse(Console.ReadLine());
+
+            int smallerNumber = FindSmallerNumber(firstNumber, secondNumber);
+            int smallestNumber = FindSmallerNumber(smallerNumber, thirdNumber);
+
+            Console.WriteLine(smallestNumber);
+        }
+
+        public static int FindSmallerNumber(int firstNumber, int secondNumber)
+        {
+            return firstNumber <= secondNumber ? firstNumber : secondNumber;
+        }
+    }
+}
---
+++ Methods - Exercise/P02-VowelsCount/P02-VowelsCount/Program.cs
@@ -0,0 +1,39 @@
+﻿namespace P02_VowelsCount
+{
+    using System;
+
+    public class Program
+    {
+        public static void Main()
+        {
+            string word = Console.ReadLine();
+            Console.WriteLine(SumVowels(word));
+        }
+
+        public static int SumVowels(string word)
+        {
+            int sum = 0;
+
+            for (int i = 0; i < word.Length; i++)
+            {
+                switch (word[i])
+                {
+                    case 'a':
+                    case 'A':
+                    case 'e':
+                    case 'E':
+                    case 'i':
+                    case 'I':
+                    case 'o':
+                    case 'O':
+                    case 'u':
+                    case 'U':
+                        sum++;
+                        break;
+                }
+            }
+
+            return sum;
+        }
+    }
+}
