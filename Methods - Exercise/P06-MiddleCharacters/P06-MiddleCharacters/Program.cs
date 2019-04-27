5d5e8aadc4fac752feca1f388834799bddead4e9
Georgi Dimitrov <43094653+Wafrey@users.noreply.github.com>
2019-04-27 12:43:20 +0300

    Add files via upload

---
+++ Methods - Exercise/P06-MiddleCharacters/P06-MiddleCharacters/Program.cs
@@ -0,0 +1,31 @@
+﻿namespace P06_MiddleCharacters
+{
+    using System;
+
+    public class Program
+    {
+        public static void Main()
+        {
+            string symbols = Console.ReadLine();
+            Console.WriteLine(PrintMiddleCharacters(symbols));
+        }
+
+        public static string PrintMiddleCharacters(string symbols)
+        {
+            string middleChars = string.Empty;
+
+            if (symbols.Length % 2 == 1)
+            {
+                middleChars += symbols[symbols.Length / 2];
+            }
+
+            else
+            {
+                middleChars += symbols[(symbols.Length / 2) - 1];
+                middleChars += symbols[symbols.Length / 2];
+            }
+
+            return middleChars;
+        }
+    }
+}
