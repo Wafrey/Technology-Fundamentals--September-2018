6ffd2e40f57b375c6179a4ea5ff4b4ae96586420
Georgi Dimitrov <43094653+Wafrey@users.noreply.github.com>
2019-04-28 18:03:05 +0300

    Add files via upload

---
+++ Methods - Exercise/P08-FactorialDivision/P08-FactorialDivision/Program.cs
@@ -0,0 +1,28 @@
+﻿namespace P08_FactorialDivision
+{
+    using System;
+
+    public class Program
+    {
+        public static void Main()
+        {
+            int firstNumber = int.Parse(Console.ReadLine());
+            int secondNumber = int.Parse(Console.ReadLine());
+
+            double result = (double)FindFactorial(firstNumber) / FindFactorial(secondNumber);
+            Console.WriteLine($"{result:F2}");
+        }
+
+        public static long FindFactorial(int number)
+        {
+            long factorial = 1;
+
+            for (int i = 2; i <= number; i++)
+            {
+                factorial *= i;
+            }
+
+            return factorial;
+        }
+    }
+}
