7b7fc74ea7090c3227d36a47a71419b98ac0de03
Georgi Dimitrov <43094653+Wafrey@users.noreply.github.com>
2019-04-28 18:02:33 +0300

    Add files via upload

---
+++ Methods - Exercise/P07-NxNMatrix/P07-NxNMatrix/Program.cs
@@ -0,0 +1,25 @@
+﻿namespace P07_NxNMatrix
+{
+    using System;
+
+    public class Program
+    {
+        public static void Main()
+        {
+            int size = int.Parse(Console.ReadLine());
+            for (int i = 0; i < size; i++)
+            {
+                PrintMatrix(size);
+                Console.WriteLine();
+            }
+        }
+
+        public static void PrintMatrix(int size)
+        {
+            for (int i = 0; i < size; i++)
+            {
+                Console.Write(size + " ");
+            }
+        }
+    }
+}
