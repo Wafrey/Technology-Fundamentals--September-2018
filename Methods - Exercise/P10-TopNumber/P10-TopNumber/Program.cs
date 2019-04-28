330b779457c9e7acb67d3f955cb69dc7b5608a96
Georgi Dimitrov <43094653+Wafrey@users.noreply.github.com>
2019-04-28 18:06:33 +0300

    Add files via upload

---
+++ Methods - Exercise/P10-TopNumber/P10-TopNumber/Program.cs
@@ -0,0 +1,53 @@
+﻿namespace P10_TopNumber
+{
+    using System;
+
+    public class Program
+    {
+        static void Main()
+        {
+            int number = int.Parse(Console.ReadLine());
+            DivisibleByEight(number);
+        }
+
+        public static void DivisibleByEight(int number)
+        {
+            for (int i = 1; i < number; i++)
+            {
+                int element = i;
+                int sum = 0;
+
+                while (element != 0)
+                {
+                    sum += element % 10;
+                    element /= 10;
+                }
+
+                if (sum % 8 == 0)
+                {
+                    bool isOdd = FindOddDigit(i);
+
+                    if (isOdd)
+                    {
+                        Console.WriteLine(i);
+                    }
+                }
+            }
+        }
+
+        public static bool FindOddDigit(int index)
+        {
+            while (index != 0)
+            {
+                if ((index % 10) % 2 == 1)
+                {
+                    return true;
+                }
+
+                index /= 10;
+            }
+
+            return false;
+        }
+    }
+}
