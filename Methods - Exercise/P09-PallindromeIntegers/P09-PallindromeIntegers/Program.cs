cc4e74afaeb3f747d75ca7b9db2001bbb329308e
Georgi Dimitrov <43094653+Wafrey@users.noreply.github.com>
2019-04-28 18:06:04 +0300

    Add files via upload

---
+++ Methods - Exercise/P09-PallindromeIntegers/P09-PallindromeIntegers/Program.cs
@@ -0,0 +1,39 @@
+﻿namespace P09_PalindromeIntegers
+{
+    using System;
+    using System.Linq;
+
+    public class Program
+    {
+        public static void Main()
+        {
+            while (true)
+            {
+                string input = Console.ReadLine();
+
+                if (input == "END")
+                {
+                    return;
+                }
+
+                char[] number = input.ToArray();
+                Console.WriteLine(CheckPalindrome(number));
+            }
+        }
+
+        public static string CheckPalindrome(char[] number)
+        {
+            char[] reversedNumber = number.Reverse().ToArray();
+
+            for (int i = 0; i < number.Length; i++)
+            {
+                if (number[i] != reversedNumber[i])
+                {
+                    return "false";
+                }
+            }
+
+            return "true";
+        }
+    }
+}
