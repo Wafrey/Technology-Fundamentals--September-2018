dd323df55ec8d6ff4e357eb5d69b5f3514aa92b7
Georgi Dimitrov <43094653+Wafrey@users.noreply.github.com>
2019-04-29 16:00:50 +0300

    Add files via upload

---
+++ Methods - More Exercise/P01-DataTypes/P01-DataTypes/Program.cs
@@ -0,0 +1,42 @@
+﻿namespace P01_DataTypes
+{
+    using System;
+
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            string dataType = Console.ReadLine();
+
+            switch (dataType)
+            {
+                case "int":
+                    Console.WriteLine(PrintInt(int.Parse(Console.ReadLine())));
+                    break;
+
+                case "real":
+                    Console.WriteLine($"{PrintDouble(double.Parse(Console.ReadLine())):F2}");
+                    break;
+
+                case "string":
+                    Console.WriteLine(PrintString(Console.ReadLine()));
+                    break;
+            }
+        }
+
+        public static int PrintInt(int input)
+        {
+            return input * 2;
+        }
+
+        public static double PrintDouble(double input)
+        {
+            return input * 1.5;
+        }
+
+        public static string PrintString(string input)
+        {
+            return $"${input}$";
+        }
+    }
+}
