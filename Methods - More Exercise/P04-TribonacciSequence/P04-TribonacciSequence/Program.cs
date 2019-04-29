256e2bd34ba988888d1479d7a1c91fe9b47c9671
Georgi Dimitrov <43094653+Wafrey@users.noreply.github.com>
2019-04-29 16:02:42 +0300

    Add files via upload

---
+++ Methods - More Exercise/P04-TribonacciSequence/P04-TribonacciSequence/Program.cs
@@ -0,0 +1,31 @@
+﻿namespace P04_TribonacciSequence
+{
+    using System;
+
+    public class Program
+    {
+        public static void Main()
+        {
+            int num = int.Parse(Console.ReadLine());
+            int[] sequence = new int[4];
+            sequence[0] = 0;
+            sequence[1] = 0;
+            sequence[2] = 1;
+            PrintTribonacciSequence(num, sequence);
+        }
+
+        public static void PrintTribonacciSequence(int num, int[] sequence)
+        {
+            for (int i = 0; i < num; i++)
+            {
+                sequence[3] = sequence[0] + sequence[1] + sequence[2];
+                Console.Write($"{sequence[2]} ");
+                sequence[0] = sequence[1];
+                sequence[1] = sequence[2];
+                sequence[2] = sequence[3];
+            }
+
+            Console.WriteLine();
+        }
+    }
+}
