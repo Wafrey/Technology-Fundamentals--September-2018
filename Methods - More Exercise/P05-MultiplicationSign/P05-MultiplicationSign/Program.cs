fcc2f03dc711e64820857024216b8814408da254
Georgi Dimitrov <43094653+Wafrey@users.noreply.github.com>
2019-04-29 16:03:09 +0300

    Add files via upload

---
+++ Methods - More Exercise/P05-MultiplicationSign/P05-MultiplicationSign/Program.cs
@@ -0,0 +1,45 @@
+﻿namespace P05_MultiplicationSign
+{
+    using System;
+
+    public class Program
+    {
+        public static void Main()
+        {
+            int[] numbers = new int[3];
+            numbers[0] = int.Parse(Console.ReadLine());
+            numbers[1] = int.Parse(Console.ReadLine());
+            numbers[2] = int.Parse(Console.ReadLine());
+            FindSign(numbers);
+        }
+
+        public static void FindSign(int[] numbers)
+        {
+            int signs = 0;
+
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                if (numbers[i] == 0)
+                {
+                    Console.WriteLine("zero");
+                    return;
+                }
+
+                else if (numbers[i] < 0)
+                {
+                    signs++;
+                }
+            }
+
+            if (signs % 2 == 0)
+            {
+                Console.WriteLine("positive");
+            }
+
+            else
+            {
+                Console.WriteLine("negative");
+            }
+        }
+    }
+}
