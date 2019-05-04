48451ed3b20611795242b86274c0967d74e76c2a
Georgi Dimitrov <43094653+Wafrey@users.noreply.github.com>
2019-05-04 14:47:39 +0300

    Add files via upload

---
+++ Lists - Exercise/AppendArrays/AppendArrays/Program.cs
@@ -0,0 +1,31 @@
+﻿namespace AppendArrays
+{
+    using System;
+    using System.Linq;
+    using System.Collections.Generic;
+
+    public class Program
+    {
+        public static void Main()
+        {
+            var numbers = Console.ReadLine().Split('|').ToList();
+            var finalList = new List<string>();
+            numbers.Reverse();
+
+            foreach (var item in numbers)
+            {
+                string[] currentNumbers = item.Split(' ');
+
+                foreach (var number in currentNumbers)
+                {
+                    if (number != "")
+                    {
+                        finalList.Add(number);
+                    }
+                }
+            }
+
+            Console.WriteLine(string.Join(" ", finalList));
+        }
+    }
+}
