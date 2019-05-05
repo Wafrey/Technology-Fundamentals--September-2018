f1b2a16d708428a60a468623209ae87e3eb022a4
Georgi Dimitrov <43094653+Wafrey@users.noreply.github.com>
2019-05-05 18:57:24 +0300

    Add files via upload

---
+++ Lists - Exercise/ChangeList/ChangeList/Program.cs
@@ -0,0 +1,35 @@
+﻿namespace ChangeList
+{
+    using System;
+    using System.Linq;
+
+    public class Program
+    {
+        public static void Main()
+        {
+            var numbers = Console.ReadLine().Split().Select(int.Parse).ToList();
+
+            while (true)
+            {
+                string[] command = Console.ReadLine().Split();
+
+                if (command[0] == "end")
+                {
+                    break;
+                }
+
+                else if (command[0] == "Delete")
+                {
+                    numbers.RemoveAll(n => n == int.Parse(command[1]));
+                }
+
+                else if (command[0] == "Insert")
+                {
+                    numbers.Insert(int.Parse(command[2]), int.Parse(command[1]));
+                }
+            }
+
+            Console.WriteLine(string.Join(" ", numbers));
+        }
+    }
+}
