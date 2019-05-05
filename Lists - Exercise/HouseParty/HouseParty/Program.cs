1a6e574c3bcfa8406490a333b6b7add787c7b0f0
Georgi Dimitrov <43094653+Wafrey@users.noreply.github.com>
2019-05-05 18:57:50 +0300

    Add files via upload

---
+++ Lists - Exercise/HouseParty/HouseParty/Program.cs
@@ -0,0 +1,50 @@
+﻿namespace HouseParty
+{
+    using System;
+    using System.Collections.Generic;
+
+    public class Program
+    {
+        public static void Main()
+        {
+            int lines = int.Parse(Console.ReadLine());
+            var partyList = new List<string>();
+
+            for (int i = 0; i < lines; i++)
+            {
+                string[] message = Console.ReadLine().Split();
+
+                if (message[2] == "going!")
+                {
+                    if (partyList.Contains(message[0]))
+                    {
+                        Console.WriteLine($"{message[0]} is already in the list!");
+                    }
+
+                    else
+                    {
+                        partyList.Add(message[0]);
+                    }
+                }
+
+                else
+                {
+                    if (!partyList.Contains(message[0]))
+                    {
+                        Console.WriteLine($"{message[0]} is not in the list!");
+                    }
+
+                    else
+                    {
+                        partyList.Remove(message[0]);
+                    }
+                }
+            }
+
+            foreach (var name in partyList)
+            {
+                Console.WriteLine(name);
+            }
+        }
+    }
+}
