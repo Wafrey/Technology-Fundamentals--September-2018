21e5293ad9837c1f95a6d7889f21591f5b9edb40
Georgi Dimitrov <43094653+Wafrey@users.noreply.github.com>
2019-05-06 14:37:25 +0300

    Add files via upload

---
+++ Lists - More Exercise/P01-Messaging/P01-Messaging/Program.cs
@@ -0,0 +1,37 @@
+﻿namespace P01_Messaging
+{
+    using System;
+    using System.Linq;
+    using System.Collections.Generic;
+
+    public class Program
+    {
+        public static void Main()
+        {
+            var numbers = Console.ReadLine().Split().Select(int.Parse).ToList();
+            string input = Console.ReadLine();
+            var text = new List<string>(input.Select(c => c.ToString()));
+            int sum = 0;
+
+            for (int i = 0; i < numbers.Count; i++)
+            {
+                while (numbers[i] != 0)
+                {
+                    sum += (numbers[i] % 10);
+                    numbers[i] /= 10;
+                }
+
+                if (sum > text.Count)
+                {
+                    sum -= (text.Count);
+                }
+
+                Console.Write(text[sum]);
+                text.RemoveAt(sum);
+                sum = 0;
+            }
+
+            Console.WriteLine();
+        }
+    }
+}
