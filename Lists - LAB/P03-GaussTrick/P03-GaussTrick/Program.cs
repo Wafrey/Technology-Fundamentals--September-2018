e369a0ad58e18a7555c0d453f8ebc8d79b73a9bf
Georgi Dimitrov <43094653+Wafrey@users.noreply.github.com>
2019-05-11 15:49:58 +0300

    Add files via upload

---
+++ Lists - LAB/P03-GaussTrick/P03-GaussTrick/Program.cs
@@ -0,0 +1,24 @@
+﻿namespace P03_GaussTrick
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+
+    public class Program
+    {
+        public static void Main()
+        {
+            List<int> numbers = Console.ReadLine().Split().Select(int.Parse).ToList();
+            int secondIndex = numbers.Count - 1;
+
+            for (int i = 0; i < secondIndex; i++)
+            {
+                numbers[i] = numbers[i] + numbers[secondIndex];
+                numbers.Remove(numbers[secondIndex]);
+                secondIndex--;
+            }
+
+            Console.WriteLine(string.Join(" ", numbers));
+        }
+    }
+}
