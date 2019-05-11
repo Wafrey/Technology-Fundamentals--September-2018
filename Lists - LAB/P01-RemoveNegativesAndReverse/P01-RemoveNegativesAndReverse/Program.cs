a3f90947c16854cfeda127a49ed1a71500a3bfc7
Georgi Dimitrov <43094653+Wafrey@users.noreply.github.com>
2019-05-11 15:48:42 +0300

    Add files via upload

---
+++ Lists - LAB/P01-RemoveNegativesAndReverse/P01-RemoveNegativesAndReverse/Program.cs
@@ -0,0 +1,26 @@
+﻿namespace P01_RemoveNegativesAndReverse
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+
+    public class Program
+    {
+        public static void Main()
+        {
+            List<int> numbers = Console.ReadLine().Split().Select(int.Parse).ToList();
+            numbers.RemoveAll(n => n < 0);
+
+            if (numbers.Count > 0)
+            {
+                numbers.Reverse();
+                Console.WriteLine(string.Join(" ", numbers));
+            }
+
+            else
+            {
+                Console.WriteLine("empty");
+            }
+        }
+    }
+}
