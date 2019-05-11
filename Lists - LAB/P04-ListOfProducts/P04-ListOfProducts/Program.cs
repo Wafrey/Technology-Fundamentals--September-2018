d62ad50210557dfe1f08acfdf7f95e60a3d1da12
Georgi Dimitrov <43094653+Wafrey@users.noreply.github.com>
2019-05-11 15:50:33 +0300

    Add files via upload

---
+++ Lists - LAB/P04-ListOfProducts/P04-ListOfProducts/Program.cs
@@ -0,0 +1,26 @@
+﻿namespace P04_ListOfProducts
+{
+    using System;
+    using System.Collections.Generic;
+
+    public class Program
+    {
+        public static void Main()
+        {
+            int lines = int.Parse(Console.ReadLine());
+            List<string> products = new List<string>();
+
+            for (int i = 0; i < lines; i++)
+            {
+                products.Add(Console.ReadLine());
+            }
+
+            products.Sort();
+
+            for (int i = 0; i < products.Count; i++)
+            {
+                Console.WriteLine($"{i + 1}.{products[i]}");
+            }
+        }
+    }
+}
