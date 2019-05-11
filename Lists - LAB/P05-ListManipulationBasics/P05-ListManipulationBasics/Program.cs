5990ec06fefc1060a07127c004e00402f4e70d84
Georgi Dimitrov <43094653+Wafrey@users.noreply.github.com>
2019-05-11 15:51:04 +0300

    Add files via upload

---
+++ Lists - LAB/P05-ListManipulationBasics/P05-ListManipulationBasics/Program.cs
@@ -0,0 +1,44 @@
+﻿namespace P05_ListManipulationBasics
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+
+    public class Program
+    {
+        public static void Main()
+        {
+            List<int> numbers = Console.ReadLine().Split().Select(int.Parse).ToList();
+
+            while (true)
+            {
+                string[] command = Console.ReadLine().Split();
+
+                if (command[0] == "end")
+                {
+                    Console.WriteLine(string.Join(" ", numbers));
+                    return;
+                }
+
+                switch (command[0])
+                {
+                    case "Add":
+                        numbers.Add(Convert.ToInt32(command[1]));
+                        break;
+
+                    case "Remove":
+                        numbers.Remove(Convert.ToInt32(command[1]));
+                        break;
+
+                    case "RemoveAt":
+                        numbers.RemoveAt(Convert.ToInt32(command[1]));
+                        break;
+
+                    case "Insert":
+                        numbers.Insert(Convert.ToInt32(command[2]), Convert.ToInt32(command[1]));
+                        break;
+                }
+            }
+        }
+    }
+}
