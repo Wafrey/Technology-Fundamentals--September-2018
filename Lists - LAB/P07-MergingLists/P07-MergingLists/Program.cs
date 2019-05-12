0665394b991c8f2b38fb53ea1f8aaf6e5f689e8d
Georgi Dimitrov <43094653+Wafrey@users.noreply.github.com>
2019-05-12 15:06:32 +0300

    Add files via upload

---
+++ Lists - LAB/P07-MergingLists/P07-MergingLists/Program.cs
@@ -0,0 +1,43 @@
+﻿namespace P07_MergingLists
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+
+    public class Program
+    {
+        public static void Main()
+        {
+            List<int> firstList = Console.ReadLine().Split().Select(int.Parse).ToList();
+            List<int> secondList = Console.ReadLine().Split().Select(int.Parse).ToList();
+            int smallerLength = Math.Min(firstList.Count, secondList.Count);
+            List<int> mixedList = new List<int>();
+
+            for (int i = 0; i < smallerLength; i++)
+            {
+                mixedList.Add(firstList[i]);
+                mixedList.Add(secondList[i]);
+            }
+
+            if (firstList.Count != secondList.Count)
+            {
+                int biggerLength = Math.Max(firstList.Count, secondList.Count);
+
+                for (int i = smallerLength; i < biggerLength; i++)
+                {
+                    if (firstList.Count > secondList.Count)
+                    {
+                        mixedList.Add(firstList[i]);
+                    }
+
+                    else if (firstList.Count < secondList.Count)
+                    {
+                        mixedList.Add(secondList[i]);
+                    }
+                }
+            }
+
+            Console.WriteLine(string.Join(" ", mixedList));
+        }
+    }
+}
