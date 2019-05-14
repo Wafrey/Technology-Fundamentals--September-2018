ee30b12ef007fb7a131bc5e5376236554c60f26d
Georgi Dimitrov <43094653+Wafrey@users.noreply.github.com>
2019-05-14 19:32:23 +0300

    Add files via upload

---
+++ Dictionaries, Lambda and LINQ - LAB/P02-OddOccurrences/P02-OddOccurrences/Program.cs
@@ -0,0 +1,39 @@
+﻿namespace P02_OddOccurrences
+{
+    using System;
+    using System.Linq;
+    using System.Collections.Generic;
+
+    public class Program
+    {
+        public static void Main()
+        {
+            string[] words = Console.ReadLine().Split().ToArray();
+            var occurrences = new Dictionary<string, int>();
+
+            foreach (string word in words)
+            {
+                string currentWord = word.ToLower();
+                if (occurrences.ContainsKey(currentWord))
+                {
+                    occurrences[currentWord]++;
+                }
+
+                else
+                {
+                    occurrences.Add(currentWord, 1);
+                }
+            }
+
+            foreach (var occurrence in occurrences)
+            {
+                if (occurrence.Value % 2 == 1)
+                {
+                    Console.Write(occurrence.Key + " ");
+                }
+            }
+
+            Console.WriteLine();
+        }
+    }
+}
