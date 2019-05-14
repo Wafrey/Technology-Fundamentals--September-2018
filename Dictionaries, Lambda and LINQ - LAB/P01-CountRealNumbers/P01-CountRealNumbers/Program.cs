c9461f0129dc846cc96830b14116c3ff56877616
Georgi Dimitrov <43094653+Wafrey@users.noreply.github.com>
2019-05-14 19:31:46 +0300

    Add files via upload

---
+++ Dictionaries, Lambda and LINQ - LAB/P01-CountRealNumbers/P01-CountRealNumbers/Program.cs
@@ -0,0 +1,33 @@
+﻿namespace P01_CountRealNumbers
+{
+    using System;
+    using System.Linq;
+    using System.Collections.Generic;
+
+    public class Program
+    {
+        public static void Main()
+        {
+            double[] numbers = Console.ReadLine().Split().Select(double.Parse).ToArray();
+            var counts = new SortedDictionary<double, int>();
+
+            foreach (double number in numbers)
+            {
+                if (counts.ContainsKey(number))
+                {
+                    counts[number]++;
+                }
+
+                else
+                {
+                    counts.Add(number, 1);
+                }
+            }
+
+            foreach (var number in counts)
+            {
+                Console.WriteLine($"{number.Key} -> {number.Value}");
+            }
+        }
+    }
+}
