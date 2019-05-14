2702bab545e1244ce8e72354cb46860d675c5b8c
Georgi Dimitrov <43094653+Wafrey@users.noreply.github.com>
2019-05-14 19:32:51 +0300

    Add files via upload

---
+++ Dictionaries, Lambda and LINQ - LAB/P03-WordSynonyms/P03-WordSynonyms/Program.cs
@@ -0,0 +1,36 @@
+﻿namespace P03_WordSynonyms
+{
+    using System;
+    using System.Collections.Generic;
+
+    public class Program
+    {
+        public static void Main()
+        {
+            int lines = int.Parse(Console.ReadLine());
+            var synonyms = new Dictionary<string, string>();
+            string[] words = new string[lines * 2];
+
+            for (int i = 0; i < lines; i++)
+            {
+                string word = Console.ReadLine();
+                string synonym = Console.ReadLine();
+
+                if (synonyms.ContainsKey(word))
+                {
+                    synonyms[word] += $", {synonym}";
+                }
+
+                else
+                {
+                    synonyms.Add(word, synonym);
+                }
+            }
+
+            foreach (var word in synonyms)
+            {
+                Console.WriteLine($"{word.Key} - {string.Join(", ", word.Value)}");
+            }
+        }
+    }
+}
