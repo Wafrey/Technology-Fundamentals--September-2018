3ef2c0fa4ca200c7313cf3ced1678f952e88e901
Georgi Dimitrov <43094653+Wafrey@users.noreply.github.com>
2019-05-14 19:33:25 +0300

    Add files via upload

---
+++ Dictionaries, Lambda and LINQ - LAB/P04-LargestThreeNumbers/P04-LargestThreeNumbers/Program.cs
@@ -0,0 +1,15 @@
+﻿namespace P04_LargestThreeNumbers
+{
+    using System;
+    using System.Linq;
+
+    public class Program
+    {
+        public static void Main()
+        {
+            var numbers = Console.ReadLine().Split().Select(int.Parse).ToList();
+            numbers = numbers.OrderByDescending(x => x).Take(3).ToList();
+            Console.WriteLine(string.Join(" ", numbers));
+        }
+    }
+}
