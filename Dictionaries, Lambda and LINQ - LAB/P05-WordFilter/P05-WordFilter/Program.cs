5d2bcfa5f71e441155e913d45d1145d0482225f1
Georgi Dimitrov <43094653+Wafrey@users.noreply.github.com>
2019-05-14 19:34:12 +0300

    Add files via upload

---
+++ Dictionaries, Lambda and LINQ - LAB/P05-WordFilter/P05-WordFilter/Program.cs
@@ -0,0 +1,18 @@
+﻿namespace P05_WordFilter
+{
+    using System;
+    using System.Linq;
+
+    public class Program
+    {
+        public static void Main()
+        {
+            var words = Console.ReadLine().Split().Where(n => n.Length % 2 == 0).ToArray();
+
+            for (int i = 0; i < words.Length; i++)
+            {
+                Console.WriteLine(words[i]);
+            }
+        }
+    }
+}
