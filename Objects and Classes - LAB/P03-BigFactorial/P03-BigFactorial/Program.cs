c19ca24671787ca77dfc9833f34be3246cf45f87
Georgi Dimitrov <43094653+Wafrey@users.noreply.github.com>
2019-05-15 18:50:24 +0300

    Add files via upload

---
+++ Objects and Classes - LAB/P03-BigFactorial/P03-BigFactorial/Program.cs
@@ -0,0 +1,21 @@
+﻿namespace P03_BigFactorial
+{
+    using System;
+    using System.Numerics;
+
+    public class Program
+    {
+        public static void Main()
+        {
+            int number = int.Parse(Console.ReadLine());
+            BigInteger factorial = 1;
+
+            for (int i = 2; i <= number; i++)
+            {
+                factorial *= i;
+            }
+
+            Console.WriteLine(factorial);
+        }
+    }
+}
