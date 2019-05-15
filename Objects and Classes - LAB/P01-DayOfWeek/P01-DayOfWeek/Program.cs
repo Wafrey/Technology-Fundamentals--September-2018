b46de718b82a1f91acca84f9f8f66f63da7f2994
Georgi Dimitrov <43094653+Wafrey@users.noreply.github.com>
2019-05-15 18:48:56 +0300

    Add files via upload

---
+++ Objects and Classes - LAB/P01-DayOfWeek/P01-DayOfWeek/Program.cs
@@ -0,0 +1,15 @@
+﻿namespace P01_DayOfWeek
+{
+    using System;
+    using System.Globalization;
+
+    public class Program
+    {
+        public static void Main()
+        {
+            string input = Console.ReadLine();
+            DateTime date = DateTime.ParseExact(input, "d-M-yyyy", CultureInfo.InvariantCulture);
+            Console.WriteLine(date.DayOfWeek);
+        }
+    }
+}
