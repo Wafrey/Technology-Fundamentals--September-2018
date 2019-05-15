80bfc87e0218bb931db78570209f80b7c8370f00
Georgi Dimitrov <43094653+Wafrey@users.noreply.github.com>
2019-05-15 18:49:52 +0300

    Add files via upload

---
+++ Objects and Classes - LAB/P02-RandomizeWords/P02-RandomizeWords/Program.cs
@@ -0,0 +1,24 @@
+﻿namespace P02_RandomizeWords
+{
+    using System;
+    using System.Linq;
+
+    public class Program
+    {
+        public static void Main()
+        {
+            var elements = Console.ReadLine().Split().ToList();
+            Random random = new Random();
+
+            for (int i = 0; i < elements.Count; i++)
+            {
+                int randomElement = random.Next(elements.Count);
+                string tempElement = elements[i];
+                elements[i] = elements[randomElement];
+                elements[randomElement] = tempElement;
+            }
+
+            Console.WriteLine(string.Join(Environment.NewLine, elements));
+        }
+    }
+}
