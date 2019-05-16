883052f467a459c47bf0939e08ed5321d015d534
Georgi Dimitrov <43094653+Wafrey@users.noreply.github.com>
2019-05-16 14:14:43 +0300

    Add files via upload

---
+++ Objects and Classes - LAB/P04-Songs/P04-Songs/Program.cs
@@ -0,0 +1,59 @@
+﻿namespace P04_Songs
+{
+    using System;
+    using System.Collections.Generic;
+
+    public class Program
+    {
+        public static void Main()
+        {
+            int songsNumber = int.Parse(Console.ReadLine());
+            var songs = new List<Songs>();
+
+            for (int i = 0; i < songsNumber; i++)
+            {
+                var input = Console.ReadLine().Split("_");
+                string type = input[0];
+                string name = input[1];
+                string time = input[2];
+
+                Songs song = new Songs();
+                song.TypeList = type;
+                song.Name = name;
+                song.Time = time;
+
+                songs.Add(song);
+            }
+
+            string typeList = Console.ReadLine();
+
+            if (typeList == "all")
+            {
+                foreach (Songs song in songs)
+                {
+                    Console.WriteLine(song.Name);
+                }
+            }
+
+            else
+            {
+                foreach (Songs song in songs)
+                {
+                    if (song.TypeList == typeList)
+                    {
+                        Console.WriteLine(song.Name);
+                    }
+                }
+            }
+        }
+    }
+
+    public class Songs
+    {
+        public string TypeList { get; set; }
+
+        public string Name { get; set; }
+
+        public string Time { get; set; }
+    }
+}
