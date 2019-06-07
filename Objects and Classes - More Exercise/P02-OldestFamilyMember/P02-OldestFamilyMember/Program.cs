66a78aa1cfc02eceb8ef070a0f5a1c034d4b7cf5
Georgi Dimitrov <43094653+Wafrey@users.noreply.github.com>
2019-06-07 13:41:58 +0300

    C# Object and Classes

---
+++ Objects and Classes - More Exercise/P02-OldestFamilyMember/P02-OldestFamilyMember/Program.cs
@@ -0,0 +1,39 @@
+﻿namespace P02_OldestFamilyMember
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+
+    public class Program
+    {
+        public static void Main()
+        {
+            int lines = int.Parse(Console.ReadLine());
+            var members = new List<Member>();
+
+            for (int i = 0; i < lines; i++)
+            {
+                var currentMember = Console.ReadLine().Split();
+
+                var member = new Member(currentMember[0], int.Parse(currentMember[1]));
+                members.Add(member);
+            }
+
+            var oldestMember = members.OrderByDescending(x => x.Age).First();
+            Console.WriteLine($"{oldestMember.Name} {oldestMember.Age}");
+        }
+    }
+
+    public class Member
+    {
+        public string Name { get; set; }
+
+        public int Age { get; set; }
+
+        public Member(string name, int age)
+        {
+            Name = name;
+            Age = age;
+        }
+    }
+}
