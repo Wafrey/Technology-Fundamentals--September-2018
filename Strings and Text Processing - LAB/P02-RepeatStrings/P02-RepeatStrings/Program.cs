0f899c2d415471ff3af7f9afc0ceeaa2f04781c5
Georgi Dimitrov <43094653+Wafrey@users.noreply.github.com>
2019-06-09 15:25:26 +0300

    C# Strings&Text Processing

---
+++ Strings and Text Processing - LAB/P02-RepeatStrings/P02-RepeatStrings/Program.cs
@@ -0,0 +1,27 @@
+﻿namespace P02_RepeatStrings
+{
+    using System;
+    using System.Text;
+
+    public class Program
+    {
+        public static void Main()
+        {
+            var words = Console.ReadLine().Split();
+
+            StringBuilder concatenatedText = new StringBuilder();
+
+            foreach (var word in words)
+            {
+                int count = word.Length;
+
+                for (int i = 0; i < count; i++)
+                {
+                    concatenatedText.Append(word);
+                }
+            }
+
+            Console.WriteLine(concatenatedText);
+        }
+    }
+}
