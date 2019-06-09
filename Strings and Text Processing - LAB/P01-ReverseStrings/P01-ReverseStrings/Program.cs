55c2934196c2d8ac609011c075f211c60ddf3396
Georgi Dimitrov <43094653+Wafrey@users.noreply.github.com>
2019-06-09 15:24:46 +0300

    C# Strings&Text Processing

---
+++ Strings and Text Processing - LAB/P01-ReverseStrings/P01-ReverseStrings/Program.cs
@@ -0,0 +1,26 @@
+﻿namespace P01_ReverseStrings
+{
+    using System;
+
+    public class Program
+    {
+        public static void Main()
+        {
+            while (true)
+            {
+                string word = Console.ReadLine();
+
+                if (word == "end")
+                {
+                    break;
+                }
+
+                char[] charArray = word.ToCharArray();
+                Array.Reverse(charArray);
+
+                string reversedWord = new string(charArray);
+                Console.WriteLine(word + " = " + reversedWord);
+            }
+        }
+    }
+}
