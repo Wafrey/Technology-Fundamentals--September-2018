5c610173660dd78b959cc2b1975d4e7dbbb19151
Georgi Dimitrov <43094653+Wafrey@users.noreply.github.com>
2019-06-09 15:25:55 +0300

    C# Strings&Text Processing

---
+++ Strings and Text Processing - LAB/P03-Substring/P03-Substring/Program.cs
@@ -0,0 +1,27 @@
+﻿namespace P03_Substring
+{
+    using System;
+
+    public class Program
+    {
+        public static void Main()
+        {
+            string firstWord = Console.ReadLine().ToLower();
+            string secondWord = Console.ReadLine().ToLower();
+            int index = 0;
+            string result = string.Empty;
+
+            while (index != -1)
+            {
+                index = secondWord.IndexOf(firstWord);
+
+                if (index != -1)
+                {
+                    secondWord = secondWord.Remove(index, firstWord.Length);
+                }
+            }
+
+            Console.WriteLine(secondWord);
+        }
+    }
+}
