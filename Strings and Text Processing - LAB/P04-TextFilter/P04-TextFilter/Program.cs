acfd4a967d45c5b969788f789ad53013d8723dd3
Georgi Dimitrov <43094653+Wafrey@users.noreply.github.com>
2019-06-11 22:59:44 +0300

    C# Strings&Text Processing

---
+++ Strings and Text Processing - LAB/P04-TextFilter/P04-TextFilter/Program.cs
@@ -0,0 +1,21 @@
+﻿namespace P04_TextFilter
+{
+    using System;
+    using System.Linq;
+
+    public class Program
+    {
+        public static void Main()
+        {
+            var banList = Console.ReadLine().Split(", ").ToList();
+            string text = Console.ReadLine();
+
+            foreach (var word in banList)
+            {
+                text = text.Replace(word, new string('*', word.Length));
+            }
+
+            Console.WriteLine(text);
+        }
+    }
+}
