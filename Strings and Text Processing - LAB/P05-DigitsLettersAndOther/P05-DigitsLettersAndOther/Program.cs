22a679cf229817309cee1c11833f3fe1a7146e50
Georgi Dimitrov <43094653+Wafrey@users.noreply.github.com>
2019-06-11 23:00:38 +0300

    C# Strings&Text Processing

---
+++ Strings and Text Processing - LAB/P05-DigitsLettersAndOther/P05-DigitsLettersAndOther/Program.cs
@@ -0,0 +1,39 @@
+﻿namespace P05_DigitsLettersAndOther
+{
+    using System;
+    using System.Text;
+
+    public class Program
+    {
+        public static void Main()
+        {
+            string input = Console.ReadLine();
+
+            StringBuilder digits = new StringBuilder();
+            StringBuilder letters = new StringBuilder();
+            StringBuilder other = new StringBuilder();
+
+            foreach (var character in input)
+            {
+                if (char.IsDigit(character))
+                {
+                    digits.Append(character);
+                }
+
+                else if (char.IsLetter(character))
+                {
+                    letters.Append(character);
+                }
+
+                else
+                {
+                    other.Append(character);
+                }
+            }
+
+            Console.WriteLine(digits);
+            Console.WriteLine(letters);
+            Console.WriteLine(other);
+        }
+    }
+}
