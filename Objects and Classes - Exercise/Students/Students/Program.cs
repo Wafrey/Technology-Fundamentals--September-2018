15aa9477a49ffc885ab23caeaf821423ded59b4b
Georgi Dimitrov <43094653+Wafrey@users.noreply.github.com>
2019-06-13 19:21:28 +0300

    C# Object and Classes

---
+++ Objects and Classes - Exercise/Students/Students/Program.cs
@@ -0,0 +1,49 @@
+﻿namespace P04_Students
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+
+    public class Program
+    {
+        public static void Main()
+        {
+            int studentsCount = int.Parse(Console.ReadLine());
+            var students = new List<Student>();
+
+            for (int i = 0; i < studentsCount; i++)
+            {
+                var currentStudent = Console.ReadLine().Split();
+                string firstName = currentStudent[0];
+                string lastName = currentStudent[1];
+                double grade = double.Parse(currentStudent[2]);
+
+                var student = new Student
+                {
+                    FirstName = firstName,
+                    LastName = lastName,
+                    Grade = grade
+                };
+
+                students.Add(student);
+            }
+
+            students = students.OrderByDescending(x => x.Grade).ToList();
+            Console.WriteLine(string.Join(Environment.NewLine, students));
+        }
+    }
+
+    public class Student
+    {
+        public string FirstName { get; set; }
+
+        public string LastName { get; set; }
+
+        public double Grade { get; set; }
+
+        public override string ToString()
+        {
+            return $"{FirstName} {LastName}: {Grade:F2}";
+        }
+    }
+}
