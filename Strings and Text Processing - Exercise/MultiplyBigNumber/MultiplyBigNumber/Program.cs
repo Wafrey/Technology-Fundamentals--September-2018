c1f776045aeae0c38ab1a716d0970a97a21afaa6
Georgi Dimitrov <43094653+Wafrey@users.noreply.github.com>
2019-06-14 16:04:49 +0300

    C# Strings and Text Processing

---
+++ Strings and Text Processing - Exercise/MultiplyBigNumber/MultiplyBigNumber/Program.cs
@@ -0,0 +1,42 @@
+﻿namespace P05_MultiplyBigNumber
+{
+    using System;
+    using System.Text;
+
+    public class Program
+    {
+        public static void Main()
+        {
+            string bigNumber = Console.ReadLine().TrimStart('0');
+            string secondNumber = Console.ReadLine().TrimStart('0');
+
+            if (secondNumber == string.Empty || bigNumber == string.Empty)
+            {
+                Console.WriteLine(0);
+                return;
+            }
+
+            int smallNumber = int.Parse(secondNumber);
+            StringBuilder result = new StringBuilder();
+            int remainder = 0;
+
+            for (int i = bigNumber.Length - 1; i >= 0; i--)
+            {
+                string symbol = bigNumber[i].ToString();
+                int digit = int.Parse(symbol);
+                int resultDigit = (digit * smallNumber) + remainder;
+                remainder = resultDigit / 10;
+                result.Append(resultDigit % 10);
+
+                if (i == 0 && remainder != 0)
+                {
+                    result.Append(remainder);
+                }
+            }
+
+            var finalResult = result.ToString().ToCharArray();
+            Array.Reverse(finalResult);
+            Console.WriteLine(string.Join("", finalResult));
+        }
+    }
+}
