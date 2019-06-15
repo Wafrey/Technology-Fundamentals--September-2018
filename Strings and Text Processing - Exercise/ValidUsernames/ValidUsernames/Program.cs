15166965a68e54e2a2f2aee47ed0674b3204c20e
Georgi Dimitrov <43094653+Wafrey@users.noreply.github.com>
2019-06-15 15:39:00 +0300

    C# Strings and Text Processing

---
+++ Strings and Text Processing - Exercise/ValidUsernames/ValidUsernames/Program.cs
@@ -0,0 +1,35 @@
+﻿namespace P01_ValidUsernames
+{
+    using System;
+
+    public class Program
+    {
+        public static void Main()
+        {
+            var passwords = Console.ReadLine().Split(", ");
+            bool isValid = true;
+
+            foreach (var password in passwords)
+            {
+                if (password.Length >= 3 && password.Length <= 16)
+                {
+                    foreach (var letter in password)
+                    {
+                        if (!char.IsLetterOrDigit(letter) && letter != '-' && letter != '_')
+                        {
+                            isValid = false;
+                            break;
+                        }
+                    }
+
+                    if (isValid)
+                    {
+                        Console.WriteLine(password);
+                    }
+
+                    isValid = true;
+                }
+            }
+        }
+    }
+}
