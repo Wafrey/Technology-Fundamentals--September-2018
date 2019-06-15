d9e8f3250a4f52249160b6fabd1e5d76eea05412
Georgi Dimitrov <43094653+Wafrey@users.noreply.github.com>
2019-06-15 15:38:26 +0300

    C# Strings and Text Processing

---
+++ Strings and Text Processing - Exercise/CharacterMultiplier/CharacterMultiplier/Program.cs
@@ -0,0 +1,48 @@
+﻿namespace P02_CharacterMultiplier
+{
+    using System;
+
+    public class Program
+    {
+        public static void Main()
+        {
+            var input = Console.ReadLine().Split();
+            string firstWord = input[0];
+            string secondWord = string.Empty;
+
+            if (input.Length > 1)
+            {
+                secondWord = input[1];
+            }
+
+            int length = Math.Min(firstWord.Length, secondWord.Length);
+            int sum = 0;
+
+            for (int i = 0; i < length; i++)
+            {
+                sum += (firstWord[i] * secondWord[i]);
+            }
+
+            if (firstWord.Length > secondWord.Length)
+            {
+                for (int i = length; i < firstWord.Length; i++)
+                {
+                    sum += firstWord[i];
+                }
+            }
+
+            else if (firstWord.Length < secondWord.Length)
+            {
+                for (int i = length; i < secondWord.Length; i++)
+                {
+                    sum += secondWord[i];
+                }
+            }
+
+            if (sum > 0)
+            {
+                Console.WriteLine(sum);
+            }
+        }
+    }
+}
