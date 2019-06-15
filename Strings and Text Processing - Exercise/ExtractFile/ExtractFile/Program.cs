e7e598e46a86c96b115f4367e3c29ed62660b581
Georgi Dimitrov <43094653+Wafrey@users.noreply.github.com>
2019-06-15 15:37:50 +0300

    C# Strings and Text Processing

---
+++ Strings and Text Processing - Exercise/ExtractFile/ExtractFile/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Text;
+
+namespace P04_CaesarCipher
+{
+    public class Program
+    {
+        public static void Main()
+        {
+            string text = Console.ReadLine();
+            StringBuilder result = new StringBuilder();
+
+            foreach (var letter in text)
+            {
+                result.Append((char)(letter + 3));
+            }
+
+            Console.WriteLine(result);
+        }
+    }
+}
