eeeda5f982ada4a2588cef3492077c27624b6a05
Georgi Dimitrov <43094653+Wafrey@users.noreply.github.com>
2019-06-17 20:06:45 +0300

    C# Strings and Text Processing

---
+++ Strings and Text Processing - More Exercise/P01-ExtractPersonInformation/P01-ExtractPersonInformation/Program.cs
@@ -0,0 +1,33 @@
+﻿namespace P01_ExtractPersonInformation
+{
+    using System;
+    using System.Text.RegularExpressions;
+
+    public class Program
+    {
+        public static void Main()
+        {
+            int lines = int.Parse(Console.ReadLine());
+            string firstRegex = @"[@\|](\w+)[@\|]";
+            string secondRegex = @"[#*](\d+)[#\*]";
+            Match matchedPerson;
+            Match matchedAge;
+
+            for (int i = 0; i < lines; i++)
+            {
+                string input = Console.ReadLine();
+
+                matchedPerson = Regex.Match(input, firstRegex);
+                matchedAge = Regex.Match(input, secondRegex);
+
+                string name = matchedPerson.Groups[1]
+                    .Value.ToString().Replace("@", "").Replace("|", "");
+
+                string age = matchedAge.Groups[1]
+                    .Value.ToString().Replace("#", "").Replace("*", "");
+
+                Console.WriteLine($"{name} is {age} years old.");
+            }
+        }
+    }
+}
