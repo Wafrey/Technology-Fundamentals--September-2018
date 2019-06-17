ea3f838eda7054bf8175607cc50f6cb62494b689
Georgi Dimitrov <43094653+Wafrey@users.noreply.github.com>
2019-06-17 20:07:22 +0300

    C# Strings and Text Processing

---
+++ Strings and Text Processing - More Exercise/P02-AsciiSumator/P02-AsciiSumator/Program.cs
@@ -0,0 +1,25 @@
+﻿namespace P02_AsciiSumator
+{
+    using System;
+
+    public class Program
+    {
+        public static void Main()
+        {
+            char startSymbol = char.Parse(Console.ReadLine());
+            char endSymbol = char.Parse(Console.ReadLine());
+            string input = Console.ReadLine();
+            int sum = 0;
+
+            for (int i = 0; i < input.Length; i++)
+            {
+                if (input[i] > startSymbol && input[i] < endSymbol)
+                {
+                    sum += input[i];
+                }
+            }
+
+            Console.WriteLine(sum);
+        }
+    }
+}
