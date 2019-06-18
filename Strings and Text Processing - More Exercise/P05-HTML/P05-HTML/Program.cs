9db72145177182d945e7b7070a69dd74adcc4922
Georgi Dimitrov <43094653+Wafrey@users.noreply.github.com>
2019-06-18 15:53:47 +0300

    C# Strings and Text Processing

---
+++ Strings and Text Processing - More Exercise/P05-HTML/P05-HTML/Program.cs
@@ -0,0 +1,42 @@
+﻿namespace P05_HTML
+{
+    using System;
+    using System.Collections.Generic;
+
+    public class Program
+    {
+        public static void Main()
+        {
+            string title = Console.ReadLine();
+            string article = Console.ReadLine();
+            var comments = new List<string>();
+
+            while (true)
+            {
+                string input = Console.ReadLine();
+
+                if (input == "end of comments")
+                {
+                    break;
+                }
+
+                comments.Add(input);
+            }
+
+            Console.WriteLine("<h1>");
+            Console.WriteLine($"    {title}");
+            Console.WriteLine("</h1>");
+
+            Console.WriteLine("<article>");
+            Console.WriteLine($"    {article}");
+            Console.WriteLine("</article>");
+
+            foreach (var comment in comments)
+            {
+                Console.WriteLine("<div>");
+                Console.WriteLine($"    {comment}");
+                Console.WriteLine("</div>");
+            }
+        }
+    }
+}
