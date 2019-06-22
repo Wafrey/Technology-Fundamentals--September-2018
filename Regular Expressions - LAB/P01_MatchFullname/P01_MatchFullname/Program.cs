2b9843982253a4381c578c1688185abf162ffd3f
Georgi Dimitrov <43094653+Wafrey@users.noreply.github.com>
2019-06-22 13:19:19 +0300

    regEx

---
+++ Regular Expressions - LAB/P01_MatchFullname/P01_MatchFullname/Program.cs
@@ -0,0 +1,21 @@
+﻿namespace P01_MatchFullName
+{
+    using System;
+    using System.Text.RegularExpressions;
+
+    public class Program
+    {
+        public static void Main()
+        {
+            string regex = @"\b[A-Z][a-z]+ [A-Z][a-z]+\b";
+            string names = Console.ReadLine();
+            MatchCollection matchedNames = Regex.Matches(names, regex);
+
+            foreach (var name in matchedNames)
+            {
+                Console.Write(name + " ");
+            }
+            Console.WriteLine();
+        }
+    }
+}
