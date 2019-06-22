ce9c222870dd9c7039a953b5f174fefefb205912
Georgi Dimitrov <43094653+Wafrey@users.noreply.github.com>
2019-06-22 13:20:25 +0300

    regEx

---
+++ Regular Expressions - LAB/P02-MatchPhoneNumber/P02-MatchPhoneNumber/Program.cs
@@ -0,0 +1,17 @@
+﻿namespace P02_MatchPhoneNumber
+{
+    using System;
+    using System.Text.RegularExpressions;
+
+    public class Program
+    {
+        public static void Main()
+        {
+            string phoneNumbers = Console.ReadLine();
+            string regex = @"\+([359]+)([-| ])2(\2)(\d{3})(\2)(\d{4})\b";
+            MatchCollection matcheNumbers = Regex.Matches(phoneNumbers, regex);
+
+            Console.WriteLine(string.Join(", ", matcheNumbers));
+        }
+    }
+}
