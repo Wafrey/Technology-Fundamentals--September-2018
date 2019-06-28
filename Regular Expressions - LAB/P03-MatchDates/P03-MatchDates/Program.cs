dec3b06cd9908ec75a8ab07e51e599255a644366
Georgi Dimitrov <43094653+Wafrey@users.noreply.github.com>
2019-06-28 15:13:57 +0300

    regEx

---
+++ Regular Expressions - LAB/P03-MatchDates/P03-MatchDates/Program.cs
@@ -0,0 +1,23 @@
+﻿namespace P03_MatchDates
+{
+    using System;
+    using System.Text.RegularExpressions;
+
+    public class Program
+    {
+        public static void Main()
+        {
+            string dates = Console.ReadLine();
+            string regex = @"\b(?<day>\d{2})([-.\/])(?<month>[A-Z][a-z]{2})\1(?<year>\d{4})\b";
+            MatchCollection matchedDates = Regex.Matches(dates, regex);
+
+            foreach (Match date in matchedDates)
+            {
+                var day = date.Groups["day"].Value;
+                var month = date.Groups["month"].Value;
+                var year = date.Groups["year"].Value;
+                Console.WriteLine($"Day: {day}, Month: {month}, Year: {year}");
+            }
+        }
+    }
+}
