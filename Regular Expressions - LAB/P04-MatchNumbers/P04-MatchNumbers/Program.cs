1716592e0b1955b93477affc4e79c52e5e9d4ff9
Georgi Dimitrov <43094653+Wafrey@users.noreply.github.com>
2019-06-30 15:29:28 +0300

    regEx

---
+++ Regular Expressions - LAB/P04-MatchNumbers/P04-MatchNumbers/Program.cs
@@ -0,0 +1,16 @@
+﻿namespace P04_MatchNumbers
+{
+    using System;
+    using System.Text.RegularExpressions;
+
+    public class Program
+    {
+        public static void Main()
+        {
+            string numbers = Console.ReadLine();
+            var regex = @"(^|(?<=\s))-?\d+(\.\d+)?($|(?=\s))";
+            MatchCollection matchedNumbers = Regex.Matches(numbers, regex);
+            Console.WriteLine(string.Join(" ", matchedNumbers));
+        }
+    }
+}
